a3efa031fa457df7d64bc1a874d5e428b7c35b0f
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-02 19:07:44 -0500

    update index.html language to Spanish, add Tailwind CSS configuration, and refactor app.component for inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: `
+    <h1 class="text-3xl font-bold text-cyan-600">{{ title }}</h1>
+  `,
+  styles: ``
 })
 export class AppComponent {
   title = 'FutScorePeru';
