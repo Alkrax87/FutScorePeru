a3e9d8ee6599d59e6720585a1c07160a7f67a106
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-06 19:51:46 -0500

    add top bar and navbar components to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TopBarComponent, NavbarComponent, RouterOutlet],
   template: `
+    <app-top-bar></app-top-bar>
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
     <h1 class="text-3xl font-bold text-cyan-600">{{ title }}</h1>
   `,
   styles: ``
