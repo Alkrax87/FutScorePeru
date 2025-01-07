a9dcc9e42d361e95efe08c584f0524551864dc91
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-06 19:07:53 -0500

    add cp-fixture component for Copa Peru

---
+++ src/app/pages/copa-peru/cp-fixture/cp-fixture.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cp-fixture',
+  imports: [],
+  templateUrl: './cp-fixture.component.html',
+  styleUrl: './cp-fixture.component.css'
+})
+export class CpFixtureComponent {
+
+}
