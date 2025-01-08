f6ba4d21f77abe71d18a0c748dc9ebaa0fcefbf2
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-08 11:20:21 -0500

    update footer component: add social media links and dynamic routing for web and leagues sections

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Facebook, Instagram, LandPlot, LucideAngularModule, Twitter } from 'lucide-angular';
+import {
+  Facebook,
+  Instagram,
+  LandPlot,
+  LucideAngularModule,
+  Twitter,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
   imports: [LucideAngularModule, RouterModule],
   templateUrl: './footer.component.html',
-  styles: ``
+  styles: ``,
 })
 export class FooterComponent {
   readonly Facebook = Facebook;
   readonly Instagram = Instagram;
   readonly Twitter = Twitter;
   readonly LandPlot = LandPlot;
+
+  routesWeb = [
+    { path: '/home', name: 'Home' },
+    { path: '/about', name: 'Acerca de' },
+    { path: '/social', name: 'Social' },
+  ];
+
+  routesLigas = [
+    { path: '/liga1', name: 'Liga 1' },
+    { path: '/liga2', name: 'Liga 2' },
+    { path: '/liga3', name: 'Liga 3' },
+    { path: '/copa-peru', name: 'Copa Perú' },
+  ];
 }
