53f2891cc052ea43d289e05471c83c64efea3483
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-17 19:33:55 -0500

    Enhance BtnComponent with new styles and hover effects for improved UI interaction

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -4,12 +4,54 @@ import { Component } from '@angular/core';
   selector: 'app-btn',
   imports: [],
   template: `
-    <button class="px-6 py-3 text-white bg-crimson rounded-full focus:outline-none">
+    <button class="switch-button bg-crimson min-w-60">
       <span>
         <ng-content></ng-content>
       </span>
     </button>
   `,
-  styles: ``,
+  styles: `
+    .switch-button {
+      border: none;
+      font-size: 16px;
+      font-weight: 700;
+      padding: 0.9rem 2rem;
+      overflow: hidden;
+      transform: skew(30deg);
+    }
+    .switch-button:hover,
+    .switch-button.active {
+      color: #fff;
+    }
+    .switch-button span {
+      transition: color 0.3s;
+      display:inline-block;
+      transform: skew(-30deg);
+    }
+    .switch-button.active span {
+      color: #fff;
+    }
+    .switch-button::before,
+    .switch-button::after {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+    .switch-button::before {
+      content: "";
+      background: #ffffff;
+      width: 120%;
+      left: -10%;
+      transition: transform 0.5s cubic-bezier(0.3, 1, 0.8, 1);
+    }
+    .switch-button:hover::before {
+      transform: translate3d(100%, 0, 0);
+    }
+    .switch-button.active::before {
+      transform: translate3d(100%, 0, 0);
+    }
+  `,
 })
 export class BtnComponent {}
