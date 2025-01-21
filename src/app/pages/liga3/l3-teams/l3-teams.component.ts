d25085a7fe9a32814503336da4050c6a14154afe
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-20 19:33:07 -0500

    Refactor L3Teams component to add card rendering

--- src/app/pages/liga3/l3-teams/l3-teams.component.ts
+++ src/app/pages/liga3/l3-teams/l3-teams.component.ts
@@ -1,27 +1,56 @@
 import { Component } from '@angular/core';
 import { FetchTeamDataService } from '../../../services/fetch-team-data.service';
+import { FetchStadiumService } from '../../../services/fetch-stadium.service';
 import { Subscription } from 'rxjs';
 import { TeamDataL3 } from '../../../interfaces/api-models/team-data-l3';
+import { TeamCard } from '../../../interfaces/ui-models/team-card';
+import { TeamCardComponent } from "../../../components/team-card/team-card.component";
 
 @Component({
   selector: 'app-l3-teams',
-  imports: [],
+  imports: [TeamCardComponent],
   templateUrl: './l3-teams.component.html',
-  styleUrl: './l3-teams.component.css'
+  styleUrl: './l3-teams.component.css',
 })
 export class L3TeamsComponent {
-  constructor(private teamsService: FetchTeamDataService) {}
+  constructor(private teamsService: FetchTeamDataService, private stadiumService: FetchStadiumService) {}
 
   private teamSubscription: Subscription | null = null;
   dataTeams: TeamDataL3[] | null = null;
+  dataTeamsCard: TeamCard[] | null = null;
 
   ngOnInit() {
     this.teamSubscription = this.teamsService.dataTeamsL3$.subscribe({
       next: (data) => {
         this.dataTeams = data;
         console.log(this.dataTeams);
+        this.getDataForCard();
+      },
+    });
+  }
+
+  async getDataForCard() {
+    const newData: TeamCard[] = [];
+
+    if (this.dataTeams) {
+      for (const team of this.dataTeams) {
+        const stadium = await this.stadiumService.fetchStadium(team.stadium.url);
+        newData.push({
+          name: team.name,
+          abbreviation: team.abbreviation,
+          image: team.imageThumbnail,
+          alt: team.alt,
+          url: team.url,
+          color: team.color,
+          stadium: {
+            name: stadium.name,
+            capacity: stadium.capacity,
+            location: stadium.location,
+          },
+        });
       }
-    })
+      this.dataTeamsCard = newData;
+    }
   }
 
   ngOnDestroy() {
