29b7b5e40422691c4c0173b574309c024fd405bc
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-27 20:14:38 -0500

    Refactor FetchMapService to inject HttpClient for improved data fetching; commented out async fetchMap method for future implementation

--- src/app/services/fetch-map.service.ts
+++ src/app/services/fetch-map.service.ts
@@ -1,10 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FetchMapService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   fetchMap(value: number) {
     return fetch('assets/json/mapL' + value + '.json')
@@ -12,4 +14,18 @@ export class FetchMapService {
       .catch((error) => console.log("Error: ", error)
       )
   }
+
+  // async fetchMap(url: string): Promise<{name: string, capacity:string, location: string}> {
+  //   try {
+  //     const response = await firstValueFrom(this.http.get<any>(url));
+  //     return {
+  //       name: response.name || 'stadium',
+  //       capacity: response.capacity || 'capacity',
+  //       location: response.location || 'location'
+  //     };
+  //   } catch (error) {
+  //     console.error('Failed to fetch stadium', error);
+  //     return { name: 'stadium', capacity: 'capacity', location: 'location' };
+  //   }
+  // }
 }
