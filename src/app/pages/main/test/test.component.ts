51b708271cbd9d4b70b219dd2e749766ddcdc13e
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-31 20:15:01 -0500

    Simplify TestComponent by removing unused service and streamlining template

--- src/app/pages/main/test/test.component.ts
+++ src/app/pages/main/test/test.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-import { StatisticsL1Service } from '../../../services/statistics-l1.service';
 
 @Component({
   selector: 'app-test',
   imports: [],
-  template: `
-    <p>
-      test works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>test works!</p> `,
+  styles: ``,
 })
-export class TestComponent {
-  constructor(private statisticsL1: StatisticsL1Service) {}
-
-  ngOnInit() {
-    this.statisticsL1.getDataForStatistics();
-  }
-}
+export class TestComponent {}
