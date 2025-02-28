7050bba78d6a7e2879f6c9973c85ed8903bad38c
Alkrax87 <miguelavp_hl@outlook.com>
2025-02-28 10:38:29 -0500

    Refactor L2FixtureComponent to integrate MatchesSetupService for fixture data handling and enhance UI layout for regional and group phases

--- src/app/pages/liga2/l2-fixture/l2-fixture.component.ts
+++ src/app/pages/liga2/l2-fixture/l2-fixture.component.ts
@@ -1,47 +1,198 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FetchDivisionService } from '../../../services/fetch-division.service';
 import { FetchTeamDataService } from '../../../services/fetch-team-data.service';
 import { FetchFixtureService } from '../../../services/fetch-fixture.service';
+import { FetchStadiumService } from '../../../services/fetch-stadium.service';
+import { FetchResultsService } from '../../../services/fetch-results.service';
+import { MatchesSetupService } from '../../../services/matches-setup.service';
 import { Subscription } from 'rxjs';
 import { TitleComponent } from "../../../components/title/title.component";
+import { BtnComponent } from "../../../components/btn/btn.component";
+import { FixtureComponent } from '../../../components/fixture/fixture.component';
 import { TeamDataL2 } from '../../../interfaces/api-models/team-data-l2';
+import { ResultsDataL2 } from '../../../interfaces/api-models/results-data-l2';
+import { FixtureDataL2 } from '../../../interfaces/api-models/fixture-data-l2';
+import { StadiumData } from '../../../interfaces/api-models/stadium-data';
 
 @Component({
   selector: 'app-l2-fixture',
-  imports: [TitleComponent],
+  imports: [TitleComponent, FixtureComponent, BtnComponent, CommonModule],
   template: `
     <app-title [title]="'Fixture'"></app-title>
-    <p>l2-fixture works!</p>
+    <div class="bg-night py-5">
+      <div class="flex justify-center">
+        <div class="w-full md:w-5/6 lg:w-6/12 grid sm:grid-cols-1 md:grid-cols-2 md:space-x-6 px-8 pb-5">
+          <app-btn (click)="setActiveTab('regional')" [active]="regional">Fase Regional</app-btn>
+          <app-btn (click)="setActiveTab('grupos')" [active]="grupos">Fase Grupos</app-btn>
+        </div>
+      </div>
+      <div class="flex justify-center px-5">
+        <div class="w-full lg:w-4/6">
+          @if (regional) {
+            @if (filteredDataForFixtureRegionalA.length > 0 && filteredDataForFixtureRegionalB.length > 0) {
+              <h3 class="text-4xl text-white font-bold">Regional Fecha {{ selectedRoundRegionalIndex + 1 }}</h3>
+              <div class="flex flex-wrap md:flex-nowrap justify-center gap-1 my-6">
+                @for (round of filteredDataForFixtureRegionalB; track $index) {
+                  <button (click)="selectedRoundRegionalIndex = $index"
+                    class="w-10 h-10 md:w-full max-w-16 text-sm bg-brightnight text-white hover:bg-crimson"
+                    [ngClass]="{'bg-crimson': selectedRoundRegionalIndex === $index}"
+                  >
+                    F{{ $index + 1}}
+                  </button>
+                }
+              </div>
+              <div class="bg-white skew-x-50 h-2 w-full mb-6"></div>
+              <h3 class="text-4xl text-white font-bold">Grupo A</h3>
+              <div class="bg-crimson skew-x-50 h-2 w-44 my-3"></div>
+              <app-fixture [data]="filteredDataForFixtureRegionalA[selectedRoundRegionalIndex ? selectedRoundRegionalIndex : 0]"></app-fixture>
+              <h3 class="text-4xl text-white font-bold mt-3">Grupo B</h3>
+              <div class="bg-crimson skew-x-50 h-2 w-44 my-3"></div>
+              <app-fixture [data]="filteredDataForFixtureRegionalB[selectedRoundRegionalIndex ? selectedRoundRegionalIndex : 0]"></app-fixture>
+            } @else {
+              <div class="flex h-64 justify-center items-center select-none">
+                <h3 class="text-3xl text-white font-bold">Fixture por definir...</h3>
+              </div>
+            }
+          }
+          @if (grupos) {
+            @if (filteredDataForFixtureGruposPromotionA.length > 0 && filteredDataForFixtureGruposPromotionB.length > 0 && filteredDataForFixtureGruposRelegation.length > 0) {
+              <h3 class="text-4xl text-white font-bold">Apertura Fecha {{ selectedRoundRegionalIndex + 1 }}</h3>
+              <div class="flex flex-wrap md:flex-nowrap justify-center gap-1 my-6">
+                @for (round of filteredDataForFixtureRegionalB; track $index) {
+                  <button (click)="selectedRoundGruposIndex = $index"
+                    class="w-10 h-10 md:w-full max-w-16 text-sm bg-brightnight text-white hover:bg-crimson"
+                    [ngClass]="{'bg-crimson': selectedRoundGruposIndex === $index}"
+                  >
+                    F{{ $index + 1}}
+                  </button>
+                }
+              </div>
+              <div class="bg-white skew-x-50 h-2 w-full mb-6"></div>
+              <h3 class="text-4xl text-white font-bold">Grupo Ascenso 1</h3>
+              <div class="bg-crimson skew-x-50 h-2 w-44 my-3"></div>
+              <app-fixture [data]="filteredDataForFixtureGruposPromotionA[selectedRoundGruposIndex ? selectedRoundGruposIndex : 0]"></app-fixture>
+              <h3 class="text-4xl text-white font-bold">Grupo Ascenso 2</h3>
+              <div class="bg-crimson skew-x-50 h-2 w-44 my-3"></div>
+              <app-fixture [data]="filteredDataForFixtureGruposPromotionB[selectedRoundGruposIndex ? selectedRoundGruposIndex : 0]"></app-fixture>
+              <h3 class="text-4xl text-white font-bold">Grupo Descenso</h3>
+              <div class="bg-crimson skew-x-50 h-2 w-44 my-3"></div>
+              <app-fixture [data]="filteredDataForFixtureGruposRelegation[selectedRoundGruposIndex ? selectedRoundGruposIndex : 0]"></app-fixture>
+            } @else {
+              <div class="flex h-64 justify-center items-center select-none">
+                <h3 class="text-3xl text-white font-bold">Fixture por definir...</h3>
+              </div>
+            }
+          }
+        </div>
+      </div>
+    </div>
   `,
   styles: ``,
 })
 export class L2FixtureComponent {
   constructor(
+    private divisionService: FetchDivisionService,
     private teamsService: FetchTeamDataService,
-    private fixtureService: FetchFixtureService
+    private fixtureService: FetchFixtureService,
+    private resultsService: FetchResultsService,
+    private stadiumService: FetchStadiumService,
+    private matchesService: MatchesSetupService
   ) {}
 
+  private divisionSubscription: Subscription | null = null;
+  private stadiumSubscription: Subscription | null = null;
   private teamSubscription: Subscription | null = null;
   private fixtureSubscription: Subscription | null = null;
-  dataTeams: TeamDataL2[] | null = null;
-  dataFixture: any;
+  private resultsSubscription: Subscription | null = null;
+  regional: boolean = false;
+  grupos: boolean = false;
+  selectedRoundRegionalIndex: number = 0;
+  selectedRoundGruposIndex: number = 0;
+  dataStadium: StadiumData[] = [];
+  dataTeams: TeamDataL2[] = [];
+  dataFixture: FixtureDataL2 | null = null;
+  dataResults: ResultsDataL2[] = [];
+  filteredDataForFixtureRegionalA: any;
+  filteredDataForFixtureRegionalB: any;
+  filteredDataForFixtureGruposPromotionA: any;
+  filteredDataForFixtureGruposPromotionB: any;
+  filteredDataForFixtureGruposRelegation: any;
+
+  setActiveTab(tab: String) {
+    this.regional = tab === 'regional';
+    this.grupos = tab === 'grupos';
+  }
 
   ngOnInit() {
-    this.teamSubscription = this.teamsService.dataTeamsL2$.subscribe({
+    this.divisionSubscription = this.divisionService.dataDivisionL2$.subscribe({
       next: (data) => {
-        this.dataTeams = data;
-        console.log(this.dataTeams);
-      },
-    });
-    this.fixtureSubscription = this.fixtureService.dataFixtureL2$.subscribe({
-      next: (data) => {
-        this.dataFixture = data;
-        console.log(this.dataFixture[0]);
+        this.regional= data ? data.stages[0].status : false;
+        this.grupos= data ? data.stages[1].status : false;
+        this.selectedRoundRegionalIndex = data ? data.stages[0].inGame - 1: 0;
+        this.selectedRoundGruposIndex = data ? data.stages[1].inGame - 1: 0;
       }
     });
+    this.stadiumSubscription = this.stadiumService.dataStadiums$.subscribe({
+      next: (data) => (this.dataStadium = data)
+    });
+    this.teamSubscription = this.teamsService.dataTeamsL2$.subscribe({
+      next: (data) => (this.dataTeams = data)
+    });
+    this.fixtureSubscription = this.fixtureService.dataFixtureL2$.subscribe({
+      next: (data) => (this.dataFixture = data)
+    });
+    this.resultsSubscription = this.resultsService.dataResultsL2$.subscribe({
+      next: (data) => (this.dataResults = data)
+    });
+    if (this.dataStadium && this.dataTeams && this.dataFixture && this.dataResults) {
+      this.getFixtureData();
+    }
+  }
+
+  getFixtureData() {
+    this.filteredDataForFixtureRegionalA = this.matchesService.transformDataForFixture(
+      this.dataTeams,
+      this.dataFixture?.regionalA,
+      this.dataResults,
+      this.dataStadium,
+      "regional"
+    );
+    this.filteredDataForFixtureRegionalB = this.matchesService.transformDataForFixture(
+      this.dataTeams,
+      this.dataFixture?.regionalB,
+      this.dataResults,
+      this.dataStadium,
+      "regional"
+    );
+    this.filteredDataForFixtureGruposPromotionA = this.matchesService.transformDataForFixture(
+      this.dataTeams,
+      this.dataFixture?.gruposPromotionA,
+      this.dataResults,
+      this.dataStadium,
+      "grupos"
+    );
+    this.filteredDataForFixtureGruposPromotionB = this.matchesService.transformDataForFixture(
+      this.dataTeams,
+      this.dataFixture?.gruposPromotionB,
+      this.dataResults,
+      this.dataStadium,
+      "grupos"
+    );
+    this.filteredDataForFixtureGruposRelegation = this.matchesService.transformDataForFixture(
+      this.dataTeams,
+      this.dataFixture?.gruposRelegation,
+      this.dataResults,
+      this.dataStadium,
+      "grupos"
+    );
   }
 
   ngOnDestroy() {
+    this.divisionSubscription?.unsubscribe();
+    this.stadiumSubscription?.unsubscribe();
     this.teamSubscription?.unsubscribe();
     this.fixtureSubscription?.unsubscribe();
+    this.resultsSubscription?.unsubscribe();
   }
 }
