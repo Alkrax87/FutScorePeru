9fe8d2a0532f728c6881ff81009b970e494059fc
Alkrax87 <miguelavp_hl@outlook.com>
2025-03-12 16:36:49 -0500

    Refactor home component to display divisions with routing and icons, removing unused CSS file

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleRight, faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styles: ``,
 })
 export class HomeComponent {
+  divisions: {
+    title: string;
+    image: string;
+    teams: number;
+    routes: string;
+  }[] = [
+    {
+      title: "Liga 1",
+      image: "assets/images/pages/liga-1.webp",
+      teams: 19,
+      routes: "/liga1"
+    },
+    {
+      title: "Liga 2",
+      image: "assets/images/pages/liga-2.webp",
+      teams: 15,
+      routes: "/liga2"
+    },
+    {
+      title: "Liga 3",
+      image: "assets/images/pages/liga-3.webp",
+      teams: 37,
+      routes: "/liga3"
+    },
+    {
+      title: "Copa Perú",
+      image: "assets/images/pages/copa-peru.webp",
+      teams: 50,
+      routes: "/copa-peru"
+    },
+  ]
 
+  Shield = faShieldHalved;
+  Arrow = faAngleRight;
 }
