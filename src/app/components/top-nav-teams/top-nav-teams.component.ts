0d9ed2b53819dd6f6823c351a2bb543cccbea7f2
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-01 18:54:54 -0500

    Update routerLink in top-nav-teams component to include category parameter

--- src/app/components/top-nav-teams/top-nav-teams.component.ts
+++ src/app/components/top-nav-teams/top-nav-teams.component.ts
@@ -20,7 +20,7 @@ import { TeamNav } from '../../interfaces/ui-models/team-nav';
           </div>
           <div class="flex-grow flex justify-between overflow-hidden md:py-2 lg:py-4">
             @for (item of teams; track $index) {
-              <a routerLink="/club/{{ item.teamId }}" class="animate-fade-up delay-75">
+              <a routerLink="club/{{ item.category }}/{{ item.teamId }}" class="animate-fade-up delay-75">
                 <img [src]="item.imageThumbnail" [alt]="item.alt" class="md:w-6 lg:w-8"/>
               </a>
             }
