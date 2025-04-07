841a2814754ef66331f4296994cb56d30c95a4e7
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-07 18:27:09 -0500

    Add best and worst goal difference properties to StatisticsData interface

--- src/app/interfaces/api-models/statistics-data.ts
+++ src/app/interfaces/api-models/statistics-data.ts
@@ -6,4 +6,6 @@ export interface StatisticsData {
   mostWins: { teamId: string; pg: number }[];
   mostDraws: { teamId: string; pe: number }[];
   mostLosses: { teamId: string; pp: number }[];
+  bestGoalDifference: { teamId: string; dg: number }[];
+  worstGoalDifference: { teamId: string; dg: number }[];
 }
