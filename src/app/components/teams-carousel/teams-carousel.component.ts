fb4eab3abcf9a24ea3ccc4db9bf47a0a0cf7e350
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-14 20:44:56 -0500

    Refactor TeamsCarouselComponent to remove unused RouterModule import and adjust marquee settings

--- src/app/components/teams-carousel/teams-carousel.component.ts
+++ src/app/components/teams-carousel/teams-carousel.component.ts
@@ -1,24 +1,21 @@
 import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { NgxMarqueeComponent } from "@omnedia/ngx-marquee";
 import { TeamCarousel } from '../../interfaces/ui-models/team-carousel';
 
 @Component({
   selector: 'app-teams-carousel',
-  imports: [NgxMarqueeComponent, RouterModule],
+  imports: [NgxMarqueeComponent],
   template: `
     <div class="w-full select-none">
-      <om-marquee [animationDuration]="duration" marqueeGap="0rem" [reverse]="reverse">
-        <div class="flex gap-5 pr-5" #OmMarqueeContent>
+      <om-marquee [animationDuration]="duration" [pauseOnHover]="false" marqueeGap="1.25rem" [reverse]="reverse">
+        <div class="flex gap-5" #OmMarqueeContent>
           @for (item of data; track $index) {
-            <a routerLink="/club/{{ item.teamId }}">
-              <div class="bg-nightfall hover:bg-crimson w-20 text-center p-4 hover:scale-110 transition-transform duration-300">
-                <div class="flex justify-center items-center w-full">
-                  <img [src]="item.image" [alt]="item.alt">
-                </div>
-                <p class="font-semibold text-sm pt-5">{{ item.abbreviation }}</p>
+            <div class="bg-nightfall w-20 text-center p-4 transition-transform duration-300">
+              <div class="flex justify-center items-center w-full">
+                <img [src]="item.image" [alt]="item.alt">
               </div>
-            </a>
+              <p class="font-semibold text-sm pt-5">{{ item.abbreviation }}</p>
+            </div>
           }
         </div>
       </om-marquee>
