d2a1c5c6e0d7fa90b9125e2357b09ef8ec8aa51e
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-19 12:19:33 -0500

    Add performance, lastgames and fixture properties to TeamInformation interface for enhanced team data structure

--- src/app/interfaces/api-models/team-information.ts
+++ src/app/interfaces/api-models/team-information.ts
@@ -20,5 +20,68 @@ export interface TeamInformation {
     capacity: number;
     location: string;
     image: string;
-  }
+  },
+  lastgames: {
+    apertura?: string[];
+    clausura?: string[];
+    acumulado?: string[];
+    regional?: string[];
+    grupos?: string[];
+    final?: string[];
+  },
+  performance: {
+    apertura?: {
+      pg: number;
+      pe: number;
+      pp: number;
+      gf: number;
+      gc: number;
+      sanction: number;
+      addition: number;
+    };
+    clausura?: {
+      pg: number;
+      pe: number;
+      pp: number;
+      gf: number;
+      gc: number;
+      sanction: number;
+      addition: number;
+    };
+    regional?: {
+      pg: number;
+      pe: number;
+      pp: number;
+      gf: number;
+      gc: number;
+      sanction: number;
+      addition: number;
+    };
+    grupos?: {
+      pg: number;
+      pe: number;
+      pp: number;
+      gf: number;
+      gc: number;
+      sanction: number;
+      addition: number;
+    };
+    final?: {
+      pg: number;
+      pe: number;
+      pp: number;
+      gf: number;
+      gc: number;
+      sanction: number;
+      addition: number;
+    };
+  },
+  fixture: {
+    apertura?: { home: string, away: string }[];
+    clausura?: { home: string, away: string }[];
+    acumulado?: { home: string, away: string }[];
+    regional?: { home: string, away: string }[];
+    grupos?: { home: string, away: string }[];
+    final?: { home: string, away: string }[];
+  },
 }
