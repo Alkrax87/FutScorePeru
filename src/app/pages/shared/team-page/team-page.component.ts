bb9ef38c874b47c098a991e2f6a792d7ac6560d8
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-20 18:03:42 -0500

    Refactor TeamPageComponent to enhance team data handling and implement dynamic category subscriptions

--- src/app/pages/shared/team-page/team-page.component.ts
+++ src/app/pages/shared/team-page/team-page.component.ts
@@ -4,13 +4,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebookF, faInstagram, faTiktok, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faCircle, faCircleCheck, faCircleMinus, faCircleXmark, faEllipsis, faFlag, faLink, faLocationDot, faRing, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FetchTeamInformationService } from '../../../services/fetch-team-information.service';
-import { FixtureData } from '../../../interfaces/api-models/fixture-data';
 import { TeamInformation } from '../../../interfaces/api-models/team-information';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, Subscription, takeUntil } from 'rxjs';
+import { FetchDivisionService } from '../../../services/fetch-division.service';
+import { BtnComponent } from "../../../components/btn/btn.component";
 
 @Component({
   selector: 'app-team-page',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, BtnComponent],
   templateUrl: './team-page.component.html',
   styles: `
     .image {
@@ -22,19 +23,77 @@ import { Subject, takeUntil } from 'rxjs';
 export class TeamPageComponent {
   constructor(
     private route: ActivatedRoute,
+    private divisionService: FetchDivisionService,
     private fetchTeamInformation: FetchTeamInformationService,
   ) {}
 
+  private divisionSubscription: Subscription | null = null;
   private destroy$ = new Subject<void>();
   teamId: string = "";
-
+  category: number = 0;
+  apertura: boolean = false;
+  clausura: boolean = false;
+  regional: boolean = false;
+  grupos: boolean = false;
+  final: boolean = false;
   teamData: TeamInformation | null = null;
+
+  setActiveTab(tab: String) {
+    this.apertura = tab === 'apertura';
+    this.clausura = tab === 'clausura';
+    this.regional = tab === 'regional';
+    this.grupos = tab === 'grupos';
+    this.final = tab === 'final';
+  }
+
+  get activeLastGames(): string | null {
+    if (this.apertura) return 'apertura';
+    if (this.clausura) return 'clausura';
+    if (this.regional) return 'regional';
+    if (this.grupos) return 'grupos';
+    if (this.final) return 'final';
+    return null;
+  }
 
   ngOnInit() {
     this.route.params.pipe(takeUntil(this.destroy$)).subscribe(params => {
       this.teamId = params['teamId'];
+      this.category = parseInt(params['category']);
+
+      switch (this.category) {
+        case 1:
+          this.divisionSubscription = this.divisionService.dataDivisionL1$.subscribe({
+            next: (data) => {
+              this.apertura = data ? data.stages[0].status : false;
+              this.clausura = data ? data.stages[1].status : false;
+            }
+          });
+          break;
+        case 2:
+          this.divisionSubscription = this.divisionService.dataDivisionL2$.subscribe({
+            next: (data) => {
+              this.regional = data ? data.stages[0].status : false;
+              this.grupos = data ? data.stages[1].status : false;
+            }
+          });
+          break;
+        case 3:
+          this.divisionSubscription = this.divisionService.dataDivisionL3$.subscribe({
+            next: (data) => {
+              this.regional = data ? data.stages[0].status : false;
+              this.final = data ? data.stages[1].status : false;
+            }
+          });
+          break;
+        default:
+          break;
+      }
+
       this.fetchTeamInformation.fetchTeamInformation(this.teamId).subscribe({
-        next: (response) => (this.teamData = response),
+        next: (response) => {
+          this.teamData = response;
+          console.log(response);
+        },
         error: (error) => console.log('Failed to fetch Team Information ', error)
       });
     });
@@ -59,5 +118,6 @@ export class TeamPageComponent {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+    this.divisionSubscription?.unsubscribe();
   }
 }
