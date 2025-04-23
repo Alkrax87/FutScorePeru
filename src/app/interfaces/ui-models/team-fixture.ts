d86f63486f82ed8451f0c251716aa7d38bbcdd1d
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-22 19:50:52 -0500

    Add TeamFixture interface to define structure for match fixtures

---
+++ src/app/interfaces/ui-models/team-fixture.ts
@@ -0,0 +1,9 @@
+export interface TeamFixture {
+  homeTeamLogo: string;
+  awayTeamLogo: string;
+  homeTeamAlt: string;
+  awayTeamAlt: string;
+  homeTeamScore: number;
+  awayTeamScore: number;
+  free: boolean;
+}
