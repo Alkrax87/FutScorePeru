881169bc13d94c87bae26d2feb18769c3f67144e
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-22 19:51:28 -0500

    Add TeamFixtureComponent to display match fixtures with dynamic data handling

---
+++ src/app/components/team-fixture/team-fixture.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input } from '@angular/core';
+import { TeamFixture } from '../../interfaces/ui-models/team-fixture';
+
+@Component({
+  selector: 'app-team-fixture',
+  imports: [],
+  template: `
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3">
+      @for (item of teamFixture; track $index) {
+        @if (item.free) {
+          <div>
+            <div class="flex flex-col items-center">
+              <p class="font-semibold text-lg">Fecha {{ $index + 1 }}</p>
+              <div class="bg-crimson skew-x-50 h-1 w-28 mb-2"></div>
+            </div>
+            <div class="bg-nightfall text-white flex gap-1 h-11">
+              <div class="w-full flex justify-center items-center font-semibold">
+                <img [src]="item.homeTeamLogo" [alt]="item.homeTeamAlt" class="w-8 mx-1"/> Descansa
+              </div>
+            </div>
+          </div>
+        } @else {
+          <div>
+            <div class="flex flex-col items-center">
+              <p class="font-semibold text-lg">Fecha {{ $index + 1 }}</p>
+              <div class="bg-crimson skew-x-50 h-1 w-28 mb-2"></div>
+            </div>
+            <div class="bg-nightfall text-white flex gap-1 h-11">
+              <div class="w-full flex justify-end items-center">
+                <img [src]="item.homeTeamLogo" [alt]="item.homeTeamAlt" class="w-8 mx-1"/>
+              </div>
+              <div class="bg-brightnight flex justify-center items-center font-bold text-3xl min-w-10 max-w-10">
+                <p>{{ item.homeTeamScore }}</p>
+              </div>
+              <div class="bg-brightnight flex justify-center items-center font-bold text-3xl min-w-10 max-w-10">
+                <p>{{ item.awayTeamScore }}</p>
+              </div>
+              <div class="w-full flex justify-start items-center">
+                <img [src]="item.awayTeamLogo" [alt]="item.awayTeamAlt" class="w-8 mx-1"/>
+              </div>
+            </div>
+          </div>
+        }
+      }
+    </div>
+  `,
+  styles: ``
+})
+export class TeamFixtureComponent {
+  @Input() teamFixture!:TeamFixture[];
+}
