35cc6b5edd6378782d1f5e95ce1686ea978ff3eb
Alkrax87 <miguelavp_hl@outlook.com>
2025-05-21 19:14:35 -0500

    Fix selectedRound variable in L2 fixture component to correctly reference grupos index

--- src/app/pages/liga2/l2-fixture/l2-fixture.component.ts
+++ src/app/pages/liga2/l2-fixture/l2-fixture.component.ts
@@ -57,7 +57,7 @@ import { StadiumData } from '../../../interfaces/api-models/stadium-data';
           }
           @if (grupos) {
             @if (filteredDataForFixtureGruposPromotionA && filteredDataForFixtureGruposPromotionB && filteredDataForFixtureGruposRelegation) {
-              <h3 class="text-4xl text-white font-bold">Grupos Fecha {{ selectedRoundRegionalIndex + 1 }}</h3>
+              <h3 class="text-4xl text-white font-bold">Grupos Fecha {{ selectedRoundGruposIndex + 1 }}</h3>
               <div class="flex flex-wrap md:flex-nowrap justify-center gap-1 my-6">
                 @for (round of filteredDataForFixtureGruposPromotionA; track $index) {
                   <button (click)="selectedRoundGruposIndex = $index"
