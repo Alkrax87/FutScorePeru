1b1a2c74c780bc805cd2318d0cf5b1662355cf42
Alkrax87 <miguelavp_hl@outlook.com>
2025-05-22 21:04:51 -0500

    Implement CpTeamsComponent with title and team card display

--- src/app/pages/copa-peru/cp-teams/cp-teams.component.ts
+++ src/app/pages/copa-peru/cp-teams/cp-teams.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../../components/title/title.component';
+import { TeamCardCp } from '../../../interfaces/ui-models/team-card-cp';
+import { TeamCardCpComponent } from '../../../components/team-card-cp/team-card-cp.component';
 
 @Component({
   selector: 'app-cp-teams',
-  imports: [],
-  template: ``,
+  imports: [TitleComponent, TeamCardCpComponent],
+  template: `
+    <app-title [title]="'Ligas'"></app-title>
+    <div class="bg-night p-5">
+      <div class="w-full">
+        <div class="flex justify-center">
+          <div class="w-full lg:w-11/12 grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            @for (item of dataTeams; track $index) {
+              <app-team-card-cp [data]="dataTeams[$index]"></app-team-card-cp>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
   styles: ``,
 })
-export class CpTeamsComponent {}
+export class CpTeamsComponent {
+  dataTeams: TeamCardCp[] = [];
+}
