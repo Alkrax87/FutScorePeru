a65b759aea387052d0279f37195455b5e2fe3d06
Alkrax87 <miguelavp_hl@outlook.com>
2025-05-25 17:51:45 -0500

    Refactor TopBarComponent to use RouterLink instead of RouterModule and enhance accessibility with focus outline

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [RouterModule],
+  imports: [RouterLink],
   template: `
     <div class="bg-night select-none">
       <div class="container mx-auto">
         <div class="flex justify-center">
           <div class="relative flex-none h-10 sm:h-[60px] border-r-[40px] sm:border-r-[60px] border-b-[40px] sm:border-b-[60px] border-r-transparent  border-b-night bg-crimson"></div>
           <div class="w-3/12 bg-crimson flex justify-center">
-            <a routerLink="/">
+            <a routerLink="/" class="focus:outline-none">
               <img class="bg-white w-10 sm:w-[60px] p-1" src="main-logo.webp" alt="main-logo"
               />
             </a>
