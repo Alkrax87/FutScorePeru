db689f3bbc0541640a6004af69758768d722f1d5
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-05-31 18:48:11 -0500

    Refactor AboutComponent to enhance layout and add content, including mission statement and statistics

--- src/app/pages/main/about/about.component.ts
+++ src/app/pages/main/about/about.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBullseye, faHandFist, faTrophy, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styles: ``,
 })
 export class AboutComponent {
-
+  Trophy = faTrophy;
+  Signal = faBullseye;
+  People = faUserGroup;
+  Hand = faHandFist
 }
