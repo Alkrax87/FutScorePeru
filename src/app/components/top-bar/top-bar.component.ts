743d728624d17b59c5b73e7e1b9fa36a28a8c8e0
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-07 20:24:53 -0500

    Enhance TopBarComponent layout: adjust visibility classes and refine border styles

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -5,17 +5,16 @@ import { RouterLink } from '@angular/router';
   selector: 'app-top-bar',
   imports: [RouterLink],
   template: `
-    <div class="bg-night select-none">
+    <div class="bg-night select-none hidden md:block">
       <div class="container mx-auto">
         <div class="flex justify-center">
-          <div class="relative flex-none h-10 sm:h-[60px] border-r-[40px] sm:border-r-[60px] border-b-[40px] sm:border-b-[60px] border-r-transparent  border-b-night bg-crimson"></div>
-          <div class="w-3/12 bg-crimson flex justify-center">
+          <div class="relative flex-none border-r-[48px] border-b-[48px] border-r-transparent border-b-night bg-crimson"></div>
+          <div class="w-2/5 lg:w-1/3 2xl:w-3/12 duration-500 bg-crimson flex justify-center">
             <a routerLink="/" class="focus:outline-none">
-              <img class="bg-white w-10 sm:w-[60px] p-1" src="main-logo.webp" alt="main-logo"
-              />
+              <img class="bg-white w-[48px] p-1" src="main-logo.webp" alt="main-logo"/>
             </a>
           </div>
-          <div class="relative flex-none h-10 sm:h-[60px] border-l-[40px] sm:border-l-[60px] border-b-[40px] sm:border-b-[60px] border-l-transparent border-b-night bg-crimson"></div>
+          <div class="relative flex-none border-l-[48px] border-b-[48px] border-l-transparent border-b-night bg-crimson"></div>
         </div>
       </div>
     </div>
