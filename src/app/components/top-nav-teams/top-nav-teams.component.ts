52e125acaaa6ad4e1e8cf7c4dd1da6d3244c0500
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-09 18:48:11 -0500

    Update routerLink logic in TopNavTeamsComponent for dynamic navigation based on team category

--- src/app/components/top-nav-teams/top-nav-teams.component.ts
+++ src/app/components/top-nav-teams/top-nav-teams.component.ts
@@ -21,7 +21,7 @@ import { TeamNav } from '../../interfaces/ui-models/team-nav';
           </div>
           <div class="flex-grow flex justify-evenly overflow-hidden md:py-2 lg:py-4">
             @for (item of teams; track $index) {
-              <a [routerLink]="['club', item.category, item.teamId]" class="animate-fade-up delay-75">
+              <a [routerLink]="[item.category === 4 ? 'liga': 'club', item.category, item.teamId]" class="animate-fade-up delay-75">
                 <img [src]="item.imageThumbnail" [alt]="item.alt" class="md:w-6 md:h-6 lg:w-8 lg:h-8 object-contain"/>
               </a>
             }
