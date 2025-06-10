fcc16be5ad55bb4f0f55960bf3a476ebbf377c47
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-10 14:26:43 -0500

    Refactor CityCardComponent template for improved styling and layout consistency

--- src/app/components/city-card/city-card.component.ts
+++ src/app/components/city-card/city-card.component.ts
@@ -6,8 +6,8 @@ import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-city-card',
   imports: [FontAwesomeModule],
   template: `
-    <div class="bg-neutral-200 dark:bg-brightnight duration-500 flex justify-between font-semibold gap-2 mx-0.5 my-1">
-      <p class="text-neutral-600 dark:text-white duration-500 text-sm pl-2 truncate my-auto">{{ city.name }}</p>
+    <div class="bg-white dark:bg-brightnight duration-500 flex justify-between font-semibold gap-1.5 mx-0.5 my-1 shadow-sm">
+      <p class="text-dark dark:text-white duration-500 text-sm pl-2 truncate my-auto">{{ city.name }}</p>
       <div class="bg-night text-white min-w-10 py-1 text-center text-sm">
         <fa-icon [icon]="Shield"></fa-icon> {{ city.teams }}
       </div>
