4c417a4018e3971b7995f1f7244bfe62364eec56
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-21 18:00:32 -0500

    Add fetchLeagueInformation method to FetchPageInformationService for league data retrieval

--- src/app/services/fetch-page-information.service.ts
+++ src/app/services/fetch-page-information.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Environments } from '../environment/environments';
 import { Observable } from 'rxjs';
 import { TeamInformation } from '../interfaces/api-models/team-information';
+import { LeagueInformation } from '../interfaces/api-models/league-information';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,10 @@ export class FetchPageInformationService {
   constructor(private http: HttpClient) {}
 
   fetchTeamInformation(teamId: string): Observable<TeamInformation> {
-    return this.http.get<TeamInformation>(this.backendUrl + '/information/' + teamId);
+    return this.http.get<TeamInformation>(this.backendUrl + '/information/club/' + teamId);
+  }
+
+  fetchLeagueInformation(leagueId: string): Observable<LeagueInformation> {
+    return this.http.get<LeagueInformation>(this.backendUrl + '/information/league/' + leagueId);
   }
 }
