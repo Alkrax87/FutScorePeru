ffaeebff1aa09a9e48774690609cea8743dd4eb7
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-21 18:07:45 -0500

    Add LeagueData interface for league information structure

---
+++ src/app/interfaces/api-models/league-data.ts
@@ -0,0 +1,19 @@
+export interface LeagueData {
+  category: number;
+  leagueId: string;
+  image: string;
+  imageThumbnail: string;
+  alt: string;
+  location: string;
+  color: {
+    c1: string;
+    c2?: string;
+  };
+  teams: {
+    teamId: string;
+    name: string;
+    abbreviation: string;
+    image: string;
+    city: string;
+  }[]
+}
