e1e0665b0861cc68c8e8bb2ddce844c7476af638
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-24 19:09:31 -0500

    Refactor LeagueInformation interface to rename 'latestRepresentatives' to 'teams' for clarity

--- src/app/interfaces/api-models/league-information.ts
+++ src/app/interfaces/api-models/league-information.ts
@@ -2,7 +2,10 @@ export interface LeagueInformation {
   title: string;
   flag: string;
   foundation: number;
-  latestRepresentatives: { name: string; image: string }[];
+  teams: {
+    name: string;
+    image: string;
+  }[],
   topWinner: { name: string; image: string; province: string; winner: number };
   leagues: string[];
   allTimeWinners: {
