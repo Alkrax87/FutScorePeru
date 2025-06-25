eec1dc07ece7198dec30b7df69f6b4dd998101a2
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-24 19:08:32 -0500

    Add LeaguePageComponent for league details

---
+++ src/app/pages/shared/league-page/league-page.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFlag, faLocationDot, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { LeagueInformation } from '../../../interfaces/api-models/league-information';
+import { FetchPageInformationService } from '../../../services/fetch-page-information.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-league-page',
+  imports: [FontAwesomeModule],
+  templateUrl: './league-page.component.html',
+  styles: `
+    .image {
+      background-image: url('/assets/images/pages/Background-pattern.png');
+      background-size: cover;
+    }
+  `,
+})
+export class LeaguePageComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private fetchInformation: FetchPageInformationService
+  ) {}
+
+  private destroy$ = new Subject<void>();
+  leagueId: string = '';
+  data: LeagueInformation | null = null;
+
+  ngOnInit() {
+    this.route.params.pipe(takeUntil(this.destroy$)).subscribe(params => {
+      this.leagueId = params['leagueId'];
+
+      this.fetchInformation.fetchLeagueInformation(this.leagueId).subscribe({
+        next: (response) => {
+          this.data = response;
+        },
+        error: (error) => console.log('Error fetching league information:', error)
+      });
+    })
+  };
+
+  Flag = faFlag;
+  Trophy = faTrophy;
+  Location = faLocationDot;
+}
