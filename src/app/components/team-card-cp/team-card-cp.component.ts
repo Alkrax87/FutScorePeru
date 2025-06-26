709325d12606861d85f48c0148b60f95caca422c
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-25 19:43:21 -0500

    Update TeamCardCpComponent template for improved layout and clarity

--- src/app/components/team-card-cp/team-card-cp.component.ts
+++ src/app/components/team-card-cp/team-card-cp.component.ts
@@ -12,27 +12,27 @@ import { RouterLink } from '@angular/router';
       <div class="flex flex-col gap-3 items-center">
         <p class="text-2xl font-semibold">{{ data?.region }}</p>
         <div [routerLink]="['../', 'liga', data?.category, data?.leagueId]">
-          <img [src]="data?.flag" alt="Flag" class="w-20 cursor-pointer">
+          <img [src]="data?.flag" alt="Flag" class="w-40 cursor-pointer">
         </div>
       </div>
       @for (team of data?.teams; track $index) {
-        <div class="bg-brightnight flex gap-3 p-3">
-          <img [src]="team.image ? team.image : 'assets/images/pages/no-team.webp'" alt="CPTeam-logo" class="w-16 h-16 min-h-16 min-w-16">
-          <div class="w-full flex flex-col justify-center">
-            <p class="font-semibold">{{ team.name }}</p>
+        <div class="bg-brightnight flex gap-1.5 sm:gap-3 p-3">
+          <img [src]="team.image ? team.image : 'assets/images/pages/no-team.webp'" alt="CPTeam-logo" class="w-12 h-12 sm:w-16 sm:h-16">
+          <div class="w-0 flex flex-1 flex-col justify-center">
+            <p class="font-semibold truncate">{{ team.name }}</p>
             <p class="text-neutral-300 text-xs">
-              <fa-icon [icon]="Location"></fa-icon> {{ team.city ? team.city : '' }}
+              <fa-icon [icon]="Location"></fa-icon> {{ team.city ? team.city : 'Por Definir' }}
             </p>
           </div>
-          <div>
+          <div class="self-center">
             @if ($index === 0) {
-              <fa-icon class="text-yellow-400" [icon]="Trophy"></fa-icon>
+              <fa-icon class="text-yellow-400" [icon]="Trophy" [title]="'Campeón'"></fa-icon>
             }
             @if ($index === 1) {
-              <fa-icon class="text-neutral-300" [icon]="Trophy"></fa-icon>
+              <fa-icon class="text-neutral-300" [icon]="Trophy" [title]="'Subcampeón'"></fa-icon>
             }
             @if ($index === 2) {
-              <fa-icon class="text-amber-600" [icon]="Trophy"></fa-icon>
+              <fa-icon class="text-amber-600" [icon]="Trophy" [title]="'Tercer Lugar'"></fa-icon>
             }
           </div>
         </div>
