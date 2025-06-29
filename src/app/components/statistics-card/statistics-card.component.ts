1eea306998f5fb3a45cc55487a7508959430ae81
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-28 20:51:08 -0500

    Refactor statistics card component for improved structure and accessibility; replace anchor tags with divs for better semantics

--- src/app/components/statistics-card/statistics-card.component.ts
+++ src/app/components/statistics-card/statistics-card.component.ts
@@ -8,39 +8,35 @@ import { RouterModule } from '@angular/router';
   selector: 'app-statistics-card',
   imports: [FontAwesomeModule, RouterModule],
   template: `
-    <div class="my-3 select-none">
+    <div class="select-none">
       @for (item of data; track $index) {
         @if ($index == 0) {
-          <a [routerLink]="['../','club', item.category, item.teamId]">
-            <div class="bg-crimson hover:bg-crimson-hover h-[136px] image rounded-t-xl flex justify-between p-2">
-              <div class="flex flex-col justify-between">
-                <div>
-                  <p class="font-bold text-xs">{{ $index + 1 }}</p>
-                  <p class="font-bold text-lg">{{ item.name }}</p>
-                </div>
-                <p class="font-bold text-5xl">{{ item.value }}</p>
+          <div [routerLink]="['../','club', item.category, item.teamId]" class="bg-crimson hover:bg-crimson-hover h-[136px] image rounded-t-xl flex justify-between p-2 cursor-pointer">
+            <div class="flex flex-col justify-between">
+              <div>
+                <p class="font-bold text-xs">{{ $index + 1 }}</p>
+                <p class="font-bold text-lg">{{ item.name }}</p>
               </div>
-              <div class="my-auto">
-                <img loading="lazy" [src]="item.image" [alt]="item.alt" class="h-24 w-24"/>
-              </div>
+              <p class="font-bold text-5xl">{{ item.value }}</p>
             </div>
-          </a>
+            <div class="my-auto">
+              <img loading="lazy" [src]="item.image" [alt]="item.alt" class="h-24 w-24"/>
+            </div>
+          </div>
         } @else {
-          <a [routerLink]="['../','club', item.category, item.teamId]">
-            <div class="bg-nightfall hover:bg-brightnight flex justify-between py-1.5 px-2">
-              <div class="flex w-full">
-                <div class="text-xs font-semibold mr-3 my-auto">{{ $index + 1 }}</div>
-                <div class="flex gap-2">
-                  <img loading="lazy" [src]="item.imageThumbnail" [alt]="item.alt" class="h-6 w-6"/>
-                  <p class="my-auto text-sm font-semibold">{{ item.name }}</p>
-                </div>
-              </div>
-              <div class="w-10 font-semibold text-lg flex justify-between">
-                <fa-icon class="flex items-center text-xxs text-gold my-1" [icon]="Arrow"></fa-icon>
-                <p class="mx-auto">{{ item.value }}</p>
+          <div [routerLink]="['../','club', item.category, item.teamId]" class="bg-nightfall hover:bg-brightnight flex justify-between py-1.5 px-2 cursor-pointer">
+            <div class="flex w-full">
+              <div class="text-xs font-semibold mr-3 my-auto">{{ $index + 1 }}</div>
+              <div class="flex gap-2">
+                <img loading="lazy" [src]="item.imageThumbnail" [alt]="item.alt" class="h-6 w-6"/>
+                <p class="my-auto text-sm font-semibold">{{ item.name }}</p>
               </div>
             </div>
-          </a>
+            <div class="w-10 font-semibold text-lg flex justify-between">
+              <fa-icon class="text-xxs text-gold" [icon]="Arrow"></fa-icon>
+              <p class="mx-auto">{{ item.value }}</p>
+            </div>
+          </div>
         }
       }
     </div>
