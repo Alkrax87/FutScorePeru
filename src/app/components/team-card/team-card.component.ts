d09d112ee4cb4ff27754454e80c256d478284ca6
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-02 20:35:08 -0500

    Refactor TeamCardComponent to remove optional chaining and ensure data is always defined

--- src/app/components/team-card/team-card.component.ts
+++ src/app/components/team-card/team-card.component.ts
@@ -9,43 +9,35 @@ import { CommonModule } from '@angular/common';
   selector: 'app-team-card',
   imports: [RouterLink, FontAwesomeModule, CommonModule],
   template: `
-    <div [routerLink]="['../', 'club', data?.category, data?.teamId]" class="flex cursor-pointer px-3 md:px-6 py-4 sm:py-8 space-x-3 duration-300"
+    <div [routerLink]="['../', 'club', data.category, data.teamId]" class="flex cursor-pointer px-3 md:px-6 py-4 sm:py-8 gap-2 duration-300"
       [ngClass]="{'bg-nightfall text-white': !isHovered}"
-      [style.backgroundColor]="isHovered ? data?.color?.c1 : ''"
-      [style.color]="isHovered ? data?.color?.c2 || '#ffffff' : ''"
+      [style.backgroundColor]="isHovered ? data.color.c1 : ''"
+      [style.color]="isHovered ? data.color.c2 || '#ffffff' : ''"
       (mouseover)="isHovered = true"
       (mouseout)="isHovered = false"
     >
       <div class="w-16 flex justify-center items-center">
-        <img loading="lazy" [src]="data?.image" [alt]="data?.alt" class="min-w-16 w-16" />
+        <img loading="lazy" [src]="data.image" [alt]="data.alt" class="min-w-16 w-16" />
       </div>
       <div class="w-full truncate">
-        <div class="w-full truncate font-semibold">
-          {{ data?.name }}
-        </div>
-        <div class="flex text-sm">
-          <div class="flex justify-center min-w-5 mr-2">
+        <div class="w-full truncate font-semibold">{{ data.name }}</div>
+        <div class="flex text-sm gap-1">
+          <div class="flex justify-center min-w-5">
             <fa-icon [icon]="Ring"></fa-icon>
           </div>
-          <div class="truncate">
-            {{ data?.stadium?.name }}
-          </div>
+          <div class="truncate">{{ data.stadium.name }}</div>
         </div>
-        <div class="flex text-xs">
-          <div class="flex justify-center min-w-5 mr-2">
+        <div class="flex text-xs gap-1">
+          <div class="flex justify-center min-w-5">
             <fa-icon [icon]="Users"></fa-icon>
           </div>
-          <div class="truncate">
-            {{ data?.stadium?.capacity }}
-          </div>
+          <div class="truncate">{{ data.stadium.capacity }}</div>
         </div>
-        <div class="flex text-xs">
-          <div class="flex justify-center min-w-5 mr-2">
+        <div class="flex text-xs gap-1">
+          <div class="flex justify-center min-w-5">
             <fa-icon [icon]="LocationDot"></fa-icon>
           </div>
-          <div class="truncate">
-            {{ data?.stadium?.location }}
-          </div>
+          <div class="truncate">{{ data.stadium.location }}</div>
         </div>
       </div>
     </div>
@@ -53,7 +45,7 @@ import { CommonModule } from '@angular/common';
   styles: ``,
 })
 export class TeamCardComponent {
-  @Input() data?: TeamCard;
+  @Input() data!: TeamCard;
   isHovered: boolean = false;
   Ring = faRing;
   Users = faUsers;
