906c8a69d097a6842b2460af5b26d6fed6107dfa
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-05 23:04:16 -0500

    Add DivisionOverviewComponent for displaying division details

---
+++ src/app/components/division-overview/division-overview.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input } from '@angular/core';
+import { DivisionData } from '../../interfaces/api-models/division-data';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendarAlt, faShieldHalved } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-division-overview',
+  imports: [FontAwesomeModule],
+  template: `
+    <div class="bg-night px-5 py-10 md:py-24 select-none duration-500">
+      <div class="flex flex-col sm:flex-row w-full lg:w-5/6 xl:w-2/3 duration-500 gap-5 mx-auto">
+        <!-- Summary -->
+        <div class="w-full sm:w-1/2 place-content-center">
+          <div class="bg-crimson text-white w-full text-center image p-3 md:p-5 duration-500">
+            <p class="text-white font-bold text-3xl md:text-4xl duration-500">
+              {{ data?.category }}<sup class="text-xl md:text-2xl duration-500">{{ data?.sup }}</sup> División
+            </p>
+          </div>
+          <div class="text-white flex gap-3 md:gap-4 mt-3 md:mt-4 duration-500">
+            <div class=" bg-nightfall text-center w-1/2 p-3 md:p-5 duration-500">
+              <fa-icon [icon]="Calendar" class="text-2xl md:text-3xl duration-500"></fa-icon>
+              <p class="text-lg md:text-xl font-semibold duration-500">{{ data?.season }}</p>
+              <p class="text-neutral-400 text-xs md:text-sm duration-500">Temporada</p>
+            </div>
+            <div class=" bg-nightfall text-center w-1/2 p-3 md:p-5 duration-500">
+              <fa-icon [icon]="Shield" class="text-2xl md:text-3xl duration-500"></fa-icon>
+              <p class="text-lg md:text-xl font-semibold duration-500">{{ data?.teams }}</p>
+              <p class="text-neutral-400 text-xs md:text-sm duration-500">Equipos</p>
+            </div>
+          </div>
+        </div>
+        <!-- About -->
+        <div class="w-full sm:w-1/2 place-content-center">
+          <p class="text-white text-2xl font-semibold">Acerca de</p>
+          <div class="bg-crimson skew-x-50 h-1.5 w-32 mt-2"></div>
+          <p class="text-neutral-300 my-4 text-justify">{{ description }}</p>
+          <div class="flex flex-wrap gap-3">
+            @for (item of tags; track $index) {
+              <p class="text-neutral-200 px-4 pb-0.5 text-sm rounded-full border-2 border-gold text-nowrap">{{ tags[$index] }}</p>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: `
+    .image {
+      background-image: url('/assets/images/pages/Background-pattern.png');
+      background-size: cover;
+    }
+  `,
+})
+export class DivisionOverviewComponent {
+  @Input() data!: DivisionData | null;
+  @Input() description!: string;
+  @Input() tags!: string[];
+  Calendar = faCalendarAlt;
+  Shield = faShieldHalved;
+}
