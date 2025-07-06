8721354119333f827df1d4802d5135de99ade5ac
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-06 10:14:29 -0500

    Refactor CityCardComponent template for improved readability and layout consistency

--- src/app/components/city-card/city-card.component.ts
+++ src/app/components/city-card/city-card.component.ts
@@ -6,14 +6,16 @@ import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-city-card',
   imports: [FontAwesomeModule],
   template: `
-    <div class="bg-white dark:bg-brightnight duration-500 flex justify-between font-semibold gap-1.5 mx-0.5 my-1 shadow-sm">
-      <p class="text-dark dark:text-white duration-500 text-sm pl-2 truncate my-auto">{{ city.name }}</p>
+    <div class="bg-white dark:bg-brightnight duration-500 flex justify-between font-semibold pl-2 gap-1.5 shadow-sm">
+      <p class="text-dark dark:text-white duration-500 text-sm  truncate my-auto">
+        {{ city.name }}
+      </p>
       <div class="bg-night text-white min-w-10 py-1 text-center text-sm">
         <fa-icon [icon]="Shield"></fa-icon> {{ city.teams }}
       </div>
     </div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class CityCardComponent {
   @Input() city!: { name: string; teams: number };
