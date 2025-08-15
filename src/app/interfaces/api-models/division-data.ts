2ba157c06fa0d2d8f2fd5a2a992f87cc705c1b83
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-14 19:34:51 -0500

    Refactor DivisionData interface to match new api data structure

--- src/app/interfaces/api-models/division-data.ts
+++ src/app/interfaces/api-models/division-data.ts
@@ -1,9 +1,31 @@
 export interface DivisionData {
-  category: number;
-  name: string;
+  divisionId: number;
   sup: string;
+  name: string;
+  image: string;
   season: number;
   teams: number;
-  image: string;
-  stages: { name: string; inGame: number; status: boolean }[];
+  firstPhase: {
+    name: string;
+    inGame: number;
+    status: boolean;
+  },
+  secondPhase: {
+    name: string;
+    inGame: number;
+    status: boolean;
+  },
+  thirdPhase: {
+    name: string;
+    status: boolean;
+  },
+  brackets: {
+    bracket32: string;
+    bracket16: string;
+    bracket8: string;
+    bracket4: string;
+    bracket2: string;
+    bracket1: string;
+    bracketExtra: string;
+  }
 }
