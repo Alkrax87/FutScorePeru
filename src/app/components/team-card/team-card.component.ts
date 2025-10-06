1b7df654fee9db842b2db8da015ea3930d7173b5
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-10-05 20:30:27 -0500

    Enhance teamCard styles and layout

--- src/app/components/team-card/team-card.component.ts
+++ src/app/components/team-card/team-card.component.ts
@@ -9,10 +9,10 @@ import { CommonModule } from '@angular/common';
   selector: 'app-team-card',
   imports: [RouterLink, FontAwesomeModule, CommonModule],
   template: `
-    <div [routerLink]="['../', 'club', data.category, data.teamId]" class="flex cursor-pointer px-3 md:px-6 py-4 sm:py-8 gap-2 duration-300"
+    <div [routerLink]="['../', 'club', data.category, data.teamId]" class="flex cursor-pointer px-3 md:px-6 py-4 sm:py-6 gap-2 duration-300"
       [ngClass]="{'bg-nightfall text-white': !isHovered}"
       [style.backgroundColor]="isHovered ? data.color.c1 : ''"
-      [style.color]="isHovered ? data.color.c2 || '#ffffff' : ''"
+      [style.color]="isHovered ? data.color.c2 + '' || '#ffffff' : ''"
       (mouseover)="isHovered = true"
       (mouseout)="isHovered = false"
     >
@@ -20,22 +20,22 @@ import { CommonModule } from '@angular/common';
         <img loading="lazy" [src]="data.image" [alt]="data.alt" class="min-w-16 w-16" />
       </div>
       <div class="w-full truncate">
-        <div class="w-full truncate font-semibold">{{ data.name }}</div>
-        <div class="flex text-sm gap-1">
-          <div class="flex justify-center min-w-5">
-            <fa-icon [icon]="Ring"></fa-icon>
+        <div class="w-full truncate font-semibold text-sm">{{ data.name }}</div>
+        <div class="flex text-xs gap-1 text-neutral-300" [style.color]="isHovered ? data.color.c2 + 'de' : ''">
+          <div class="flex justify-center min-w-4">
+            <fa-icon [icon]="Ring" class="text-xs"></fa-icon>
           </div>
           <div class="truncate">{{ data.stadium.name }}</div>
         </div>
-        <div class="flex text-xs gap-1">
-          <div class="flex justify-center min-w-5">
-            <fa-icon [icon]="Users"></fa-icon>
+        <div class="flex text-xs gap-1 text-neutral-300" [style.color]="isHovered ? data.color.c2 + 'de' : ''">
+          <div class="flex justify-center min-w-4">
+            <fa-icon [icon]="Users" class="text-xs"></fa-icon>
           </div>
-          <div class="truncate">{{ data.stadium.capacity }}</div>
+          <div class="truncate">{{ formatNumber(data.stadium.capacity!) }}</div>
         </div>
-        <div class="flex text-xs gap-1">
-          <div class="flex justify-center min-w-5">
-            <fa-icon [icon]="LocationDot"></fa-icon>
+        <div class="flex text-xs gap-1 text-neutral-300" [style.color]="isHovered ? data.color.c2 + 'de' : ''">
+          <div class="flex justify-center min-w-4">
+            <fa-icon [icon]="LocationDot" class="text-xs"></fa-icon>
           </div>
           <div class="truncate">{{ data.stadium.location }}</div>
         </div>
@@ -50,4 +50,8 @@ export class TeamCardComponent {
   Ring = faRing;
   Users = faUsers;
   LocationDot = faLocationDot;
+
+  formatNumber(num: number) {
+    return new Intl.NumberFormat("en-US").format(num);
+  }
 }
