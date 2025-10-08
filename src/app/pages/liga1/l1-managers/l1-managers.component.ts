6d563de6fbf76408b043395958218e26cde9cd8b
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-10-08 10:28:00 -0500

    Refactor managers components for improved layout and styling consistency

--- src/app/pages/liga1/l1-managers/l1-managers.component.ts
+++ src/app/pages/liga1/l1-managers/l1-managers.component.ts
@@ -15,16 +15,18 @@ import { ManagerCarousel } from '../../../interfaces/ui-models/manager-carousel'
   imports: [TitleComponent, ManagerCarouselComponent, RouterLink],
   template: `
     <app-title [title]="'Técnicos'"></app-title>
-    <div class="bg-night p-5 select-none">
+    <div class="bg-night p-3 sm:p-5 duration-500 select-none">
       <div class="flex justify-center">
-        <div class="w-full md:w-4/5 xl:w-1/2 flex flex-col gap-5">
+        <div class="w-full md:w-4/5 xl:w-1/2 flex flex-col gap-3 sm:gap-5 duration-500">
           @for (item of dataCarousel; track $index) {
             <div>
-              <div class="flex w-fit items-center gap-2 cursor-pointer" [routerLink]="['../club', item.category, item.teamId]">
-                <img [src]="item.image" [alt]="item.alt" class="w-8 md:w-12">
-                <p class="text-white font-semibold text-sm md:text-xl">{{ item.name }}</p>
+              <div class="w-fit">
+                <div class="flex gap-2 items-center cursor-pointer" [routerLink]="['../club', item.category, item.teamId]">
+                  <img [src]="item.image" [alt]="item.alt" class="w-10" />
+                  <p class="text-lg text-white font-bold">{{ item.name }}</p>
+                </div>
+                <div class="bg-crimson skew-x-50 h-1.5 mt-1 mb-2"></div>
               </div>
-              <div class="bg-crimson skew-x-50 h-1.5 md:h-2 w-36 md:w-56 my-3"></div>
               <app-manager-carousel [data]="item.manager"></app-manager-carousel>
             </div>
           }
@@ -49,10 +51,10 @@ export class L1ManagersComponent {
 
   ngOnInit() {
     this.teamSubscription = this.teamsService.dataTeamsL1$.subscribe({
-      next: (data) => (this.dataTeams = data)
+      next: (data) => (this.dataTeams = data),
     });
     this.managerSubscription = this.managerService.dataManagersL1$.subscribe({
-      next: (data) => (this.dataManagers = data)
+      next: (data) => (this.dataManagers = data),
     });
 
     if (this.dataTeams && this.dataManagers) {
