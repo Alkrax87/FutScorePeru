57fe6f018688077bf76a8f993310dbf72a6ff13a
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-10-18 17:38:58 -0500

    Add FixtureCompactCard interface to define fixture data structure for home fixture

---
+++ src/app/interfaces/ui-models/fixture-compact-card.ts
@@ -0,0 +1,10 @@
+export interface FixtureCompactCard {
+  homeTeamAbbreviation: string;
+  awayTeamAbbreviation: string;
+  homeTeamImageThumbnail: string;
+  awayTeamImageThumbnail: string;
+  homeTeamAlt: string;
+  awayTeamAlt: string;
+  homeTeamResult: string | number;
+  awayTeamResult: string | number;
+}
