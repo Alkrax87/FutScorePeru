6d8786784575bce28de80b0ea69fa89ef63d435f
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-10-19 19:48:37 -0500

    Refactor transformDataForHomeFixture to support multiple stages and optional property for results

--- src/app/services/matches-setup.service.ts
+++ src/app/services/matches-setup.service.ts
@@ -138,35 +138,38 @@ export class MatchesSetupService {
     teams: TeamData[],
     fixture: any,
     results: ResultsData[],
-    stage: string,
-    inGame: number
+    stages: string[],
+    inGame: number,
+    property?: string,
   ) {
     const teamMap = new Map(teams.map((team) => [team.teamId, team]));
     const resultsMap = new Map(results.map((result: any) => [result.teamId, result]));
 
     const mappedData = [];
 
-    for (const element of fixture[stage][inGame - 1]) {
-      const homeTeam = teamMap.get(element.home);
-      const awayTeam = teamMap.get(element.away);
+    for (const stage of stages) {
+      for (const element of fixture[stage][inGame - 1]) {
+        const homeTeam = teamMap.get(element.home);
+        const awayTeam = teamMap.get(element.away);
 
-      if (homeTeam && awayTeam) {
-        const homeResults = resultsMap.get(homeTeam.teamId);
-        const awayResults = resultsMap.get(awayTeam.teamId);
+        if (homeTeam && awayTeam) {
+          const homeResults = resultsMap.get(homeTeam.teamId);
+          const awayResults = resultsMap.get(awayTeam.teamId);
 
-        const resultHome = homeResults[stage]?.[inGame - 1] ?? "";
-        const resultAway = awayResults[stage]?.[inGame - 1] ?? "";
+          const resultAway = awayResults[property ? property : stage]?.[inGame - 1] ?? "";
+          const resultHome = homeResults[property ? property : stage]?.[inGame - 1] ?? "";
 
-        mappedData.push({
-          homeTeamAbbreviation: homeTeam.abbreviation,
-          awayTeamAbbreviation: awayTeam.abbreviation,
-          homeTeamImageThumbnail: homeTeam.imageThumbnail,
-          awayTeamImageThumbnail: awayTeam.imageThumbnail,
-          homeTeamAlt: homeTeam.alt,
-          awayTeamAlt: awayTeam.alt,
-          homeTeamResult: resultHome,
-          awayTeamResult: resultAway,
-        });
+          mappedData.push({
+            homeTeamAbbreviation: homeTeam.abbreviation,
+            awayTeamAbbreviation: awayTeam.abbreviation,
+            homeTeamImageThumbnail: homeTeam.imageThumbnail,
+            awayTeamImageThumbnail: awayTeam.imageThumbnail,
+            homeTeamAlt: homeTeam.alt,
+            awayTeamAlt: awayTeam.alt,
+            homeTeamResult: resultHome,
+            awayTeamResult: resultAway,
+          });
+        }
       }
     }
 
