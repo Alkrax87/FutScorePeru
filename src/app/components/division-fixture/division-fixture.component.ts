533b9218da4cc2fe28b9852202981db85e94e93b
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-11-04 22:07:23 -0500

    Refactor DivisionFixtureComponent to enhance carousel display and improve fixture rendering

--- src/app/components/division-fixture/division-fixture.component.ts
+++ src/app/components/division-fixture/division-fixture.component.ts
@@ -1,61 +1,127 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
-import { NgxMarqueeComponent } from "@omnedia/ngx-marquee";
+import { faAngleDoubleRight, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FixtureCompactCard } from '../../interfaces/ui-models/fixture-compact-card';
 
 @Component({
   selector: 'app-division-fixture',
-  imports: [RouterLink, FontAwesomeModule, NgxMarqueeComponent],
+  imports: [RouterLink, FontAwesomeModule],
   template: `
-    @if (stageData && fixture && duration) {
-      <div class="bg-night select-none w-full px-3 sm:px-5 py-2 duration-500">
-        <div class="flex text-neutral-300 font-bold text-xs gap-2">
-          <div class="flex gap-1">
-            <p>{{ stageData.name }}</p>
-            <p>F{{ stageData.inGame }}</p>
-          </div>
-          <div class="bg-neutral-300 my-auto h-0.5 w-full rounded-full"></div>
-        </div>
-        <om-marquee [animationDuration]="duration" [pauseOnHover]="false" class="[mask-image:linear-gradient(to_right,transparent_0%,black_5%,black_95%,transparent_100%)]">
-          <div class="flex justify-center gap-4" #OmMarqueeContent>
-            @for (match of fixture; track $index) {
-              <div class="bg-nightfall h-14 flex">
-                <div class="w-14 p-2">
-                  <img [src]="match.homeTeamImageThumbnail" [alt]="match.homeTeamAlt" class="h-full"/>
-                </div>
-                <div class="flex gap-1">
-                  <div class="bg-brightnight text-white flex flex-col justify-center items-center font-bold min-w-11 max-w-11">
-                    <p class="text-xs">{{ match.homeTeamAbbreviation }}</p>
-                    <p class="text-3xl h-9">{{ match.homeTeamResult }}</p>
+    @if (fixture) {
+      <div class="bg-night px-3 sm:px-5 select-none duration-500 h-20">
+        <div class="relative w-full -top-10 md:-top-12 duration-500">
+          <div class="flex carousel mb-2 overflow-x-hidden">
+            <div class="flex gap-4 pr-4 group" [style.animation-duration]="animationDuration">
+              @for (item of fixture; track $index) {
+                <div class="bg-nightfall flex-shrink-0 w-44 md:w-60 duration-500">
+                  <div class="bg-crimson h-1"></div>
+                  <div class="flex justify-between p-3 gap-1">
+                    <!-- Left -->
+                    <div>
+                      <img [src]="item.homeTeamImageThumbnail" [alt]="item.homeTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.homeTeamAbbreviation }}</p>
+                    </div>
+                    <!-- Results -->
+                    <div class="my-auto">
+                      <div class="flex gap-1 text-xl md:text-3xl font-bold duration-500">
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.homeTeamResult }}</div>
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.awayTeamResult }}</div>
+                      </div>
+                    </div>
+                    <!-- Right -->
+                    <div>
+                      <img [src]="item.awayTeamImageThumbnail" [alt]="item.awayTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.awayTeamAbbreviation }}</p>
+                     </div>
                   </div>
-                  <div class="bg-brightnight text-white flex flex-col justify-center items-center font-bold min-w-11 max-w-11">
-                    <p class="text-xs">{{ match.awayTeamAbbreviation }}</p>
-                    <p class="text-3xl h-9">{{ match.awayTeamResult }}</p>
+                </div>
+              }
+            </div>
+            <div aria-hidden class="flex gap-4 pr-4 group" [style.animation-duration]="animationDuration">
+              @for (item of fixture; track $index) {
+                <div class="bg-nightfall flex-shrink-0 w-44 md:w-60 duration-500">
+                  <div class="bg-crimson h-1"></div>
+                  <div class="flex justify-between p-3 gap-1">
+                    <!-- Left -->
+                    <div>
+                      <img [src]="item.homeTeamImageThumbnail" [alt]="item.homeTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.homeTeamAbbreviation }}</p>
+                    </div>
+                    <!-- Results -->
+                    <div class="my-auto">
+                      <div class="flex gap-1 text-xl md:text-3xl font-bold duration-500">
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.homeTeamResult }}</div>
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.awayTeamResult }}</div>
+                      </div>
+                    </div>
+                    <!-- Right -->
+                    <div>
+                      <img [src]="item.awayTeamImageThumbnail" [alt]="item.awayTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.awayTeamAbbreviation }}</p>
+                     </div>
                   </div>
                 </div>
-                <div class="w-14 p-2">
-                  <img [src]="match.awayTeamImageThumbnail" [alt]="match.awayTeamAlt" class="h-full"/>
+              }
+            </div>
+            <div aria-hidden class="flex gap-4 pr-4 group" [style.animation-duration]="animationDuration">
+              @for (item of fixture; track $index) {
+                <div class="bg-nightfall flex-shrink-0 w-44 md:w-60 duration-500">
+                  <div class="bg-crimson h-1"></div>
+                  <div class="flex justify-between p-3 gap-1">
+                    <!-- Left -->
+                    <div>
+                      <img [src]="item.homeTeamImageThumbnail" [alt]="item.homeTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.homeTeamAbbreviation }}</p>
+                    </div>
+                    <!-- Results -->
+                    <div class="my-auto">
+                      <div class="flex gap-1 text-xl md:text-3xl font-bold duration-500">
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.homeTeamResult }}</div>
+                        <div class="bg-brightnight text-white flex justify-center items-center w-8 md:w-12 h-8 md:h-12 duration-500">{{ item.awayTeamResult }}</div>
+                      </div>
+                    </div>
+                    <!-- Right -->
+                    <div>
+                      <img [src]="item.awayTeamImageThumbnail" [alt]="item.awayTeamAlt" class="w-8 md:w-12 duration-500"/>
+                      <p class="text-neutral-100 text-center text-xs md:text-sm font-semibold duration-500">{{ item.awayTeamAbbreviation }}</p>
+                     </div>
+                  </div>
                 </div>
-              </div>
-            }
+              }
+            </div>
           </div>
-        </om-marquee>
-        <div class="font-bold text-sm flex justify-end">
-          <span [routerLink]="'../fixture'" class="text-crimson hover:text-crimson-hover cursor-pointer">
-            FIXTURE COMPLETO <fa-icon [icon]="Arrow"></fa-icon>
-          </span>
+          <div class="font-bold text-xs md:text-sm flex justify-end duration-500">
+            <span [routerLink]="'../fixture'" class="text-crimson hover:text-crimson-hover cursor-pointer">
+              FIXTURE COMPLETO <fa-icon [icon]="Arrow"></fa-icon>
+            </span>
+          </div>
         </div>
       </div>
     }
   `,
-  styles: ``,
+  styles: `
+    .carousel::-webkit-scrollbar {
+      display: none;
+    }
+    .group {
+      animation: carousel infinite linear;
+      will-change: transform;
+    }
+    @keyframes carousel {
+      from { transform: translateX(0); }
+      to { transform: translateX(-100%); }
+    }
+  `,
 })
 export class DivisionFixtureComponent {
-  @Input() stageData!: { name: string, inGame: number };
   @Input() fixture!: FixtureCompactCard[];
-  @Input() duration!: string;
 
+  Left = faArrowLeft;
+  Right = faArrowRight;
   Arrow = faAngleDoubleRight;
+
+  get animationDuration(): string {
+    return `${this.fixture.length * 10}s`;
+  }
 }
