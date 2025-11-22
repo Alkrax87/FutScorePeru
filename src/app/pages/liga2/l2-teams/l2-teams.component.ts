5adc7981b5fce7d101e1284c2ff2c94e1f17b7c6
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-11-22 16:11:47 -0500

    Refactor team components to use dependency injection and combine data streams for improved performance

--- src/app/pages/liga2/l2-teams/l2-teams.component.ts
+++ src/app/pages/liga2/l2-teams/l2-teams.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FetchTeamDataService } from '../../../services/fetch-team-data.service';
 import { FetchStadiumService } from '../../../services/fetch-stadium.service';
 import { UiDataMapperService } from '../../../services/ui-data-mapper.service';
-import { Subscription } from 'rxjs';
+import { combineLatest } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TitleComponent } from '../../../components/title/title.component';
 import { TeamCardComponent } from '../../../components/team-card/team-card.component';
-import { TeamData } from '../../../interfaces/api-models/team-data';
-import { StadiumData } from '../../../interfaces/api-models/stadium-data';
 import { TeamCard } from '../../../interfaces/ui-models/team-card';
 
 @Component({
@@ -15,45 +14,25 @@ import { TeamCard } from '../../../interfaces/ui-models/team-card';
   template: `
     <app-title [title]="'Clubes'"></app-title>
     <div class="bg-night px-3 sm:px-5 py-10 lg:py-16 duration-500 select-none">
-      <div class="flex justify-center">
-        <div class="w-full lg:w-11/12 xl:w-10/12 grid gap-3 sm:gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 duration-500">
-          @for (item of dataTeamsCard; track $index) {
-            <app-team-card [data]="item"></app-team-card>
-          }
-        </div>
+      <div class="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(310px,_1fr))] gap-3 sm:gap-5 max-w-screen-xl mx-auto duration-500">
+        @for (item of dataTeamsCard; track $index) {
+          <app-team-card [data]="item"></app-team-card>
+        }
       </div>
     </div>
   `,
   styles: ``,
 })
 export class L2TeamsComponent {
-  constructor(
-    private teamsService: FetchTeamDataService,
-    private stadiumService: FetchStadiumService,
-    private uiDataMapperService: UiDataMapperService
-  ) {}
+  private teamsService = inject(FetchTeamDataService);
+  private stadiumService = inject(FetchStadiumService);
+  private uiDataMapperService = inject(UiDataMapperService);
 
-  private teamSubscription: Subscription | null = null;
-  private stadiumSubscription: Subscription | null = null;
-  dataTeams: TeamData[] = [];
-  dataStadiums: StadiumData[] = [];
   dataTeamsCard: TeamCard[] = [];
 
-  ngOnInit() {
-    this.teamSubscription = this.teamsService.dataTeamsL2$.subscribe({
-      next: (data) => (this.dataTeams = data),
+  constructor() {
+    combineLatest([this.teamsService.dataTeamsL2$, this.stadiumService.dataStadiums$,]).pipe(takeUntilDestroyed()).subscribe({
+      next: ([teams, stadiums]) => this.dataTeamsCard = this.uiDataMapperService.teamCardMapper(teams, stadiums)
     });
-    this.stadiumSubscription = this.stadiumService.dataStadiums$.subscribe({
-      next: (data) => (this.dataStadiums = data),
-    });
-
-    if (this.dataTeams && this.dataStadiums) {
-      this.dataTeamsCard = this.uiDataMapperService.teamCardMapper(this.dataTeams, this.dataStadiums);
-    }
-  }
-
-  ngOnDestroy() {
-    this.teamSubscription?.unsubscribe();
-    this.stadiumSubscription?.unsubscribe();
   }
 }
