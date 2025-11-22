30e0de10a58163f6c7ef0f1d87a6d8b5879a4ac0
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-11-22 16:28:01 -0500

    Refactor L1 and L2 Managers components to use dependency injection and combine data streams for improved performance

--- src/app/pages/liga2/l2-managers/l2-managers.component.ts
+++ src/app/pages/liga2/l2-managers/l2-managers.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FetchTeamDataService } from '../../../services/fetch-team-data.service';
 import { FetchManagerService } from '../../../services/fetch-manager.service';
 import { UiDataMapperService } from '../../../services/ui-data-mapper.service';
-import { Subscription } from 'rxjs';
+import { combineLatest } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TitleComponent } from '../../../components/title/title.component';
 import { ManagerCarouselComponent } from '../../../components/manager-carousel/manager-carousel.component';
-import { TeamData } from '../../../interfaces/api-models/team-data';
-import { ManagerData } from '../../../interfaces/api-models/manager-data';
 import { ManagerCarousel } from '../../../interfaces/ui-models/manager-carousel';
 
 @Component({
@@ -16,54 +15,34 @@ import { ManagerCarousel } from '../../../interfaces/ui-models/manager-carousel'
   template: `
     <app-title [title]="'Técnicos'"></app-title>
     <div class="bg-night px-3 sm:px-5 py-10 lg:py-16 duration-500 select-none">
-      <div class="flex justify-center">
-        <div class="w-full md:w-4/5 xl:w-1/2 flex flex-col gap-3 sm:gap-5 duration-500">
-          @for (item of dataCarousel; track $index) {
-            <div>
-              <div class="w-fit">
-                <div class="flex gap-2 items-center cursor-pointer" [routerLink]="['../club', item.category, item.teamId]">
-                  <img [src]="item.image" [alt]="item.alt" class="w-10" />
-                  <p class="text-lg text-white font-bold">{{ item.name }}</p>
-                </div>
-                <div class="bg-crimson skew-x-50 h-1.5 mt-1 mb-2"></div>
+      <div class="max-w-screen-lg mx-auto flex flex-col gap-3 sm:gap-5 duration-500">
+        @for (item of dataCarousel; track $index) {
+          <div>
+            <div class="w-fit">
+              <div class="flex gap-2 items-center cursor-pointer" [routerLink]="['../club', item.category, item.teamId]">
+                <img [src]="item.image" [alt]="item.alt" class="w-10" />
+                <p class="text-lg text-white font-bold">{{ item.name }}</p>
               </div>
-              <app-manager-carousel [data]="item.manager"></app-manager-carousel>
+              <div class="bg-crimson skew-x-50 h-1.5 mt-1 mb-2"></div>
             </div>
-          }
-        </div>
+            <app-manager-carousel [data]="item.manager"></app-manager-carousel>
+          </div>
+        }
       </div>
     </div>
   `,
   styles: ``,
 })
 export class L2ManagersComponent {
-  constructor(
-    private teamsService: FetchTeamDataService,
-    private managerService: FetchManagerService,
-    private uiDataMapperService: UiDataMapperService
-  ) {}
+  private teamsService = inject(FetchTeamDataService);
+  private managerService = inject(FetchManagerService);
+  private uiDataMapperService = inject(UiDataMapperService);
 
-  private teamSubscription: Subscription | null = null;
-  private managerSubscription: Subscription | null = null;
-  dataTeams: TeamData[] = [];
-  dataManagers: ManagerData[] = [];
   dataCarousel: ManagerCarousel[] = [];
 
-  ngOnInit() {
-    this.teamSubscription = this.teamsService.dataTeamsL2$.subscribe({
-      next: (data) => (this.dataTeams = data),
+  constructor() {
+    combineLatest([this.teamsService.dataTeamsL2$, this.managerService.dataManagersL2$]).pipe(takeUntilDestroyed()).subscribe({
+      next: ([teams, managers]) => this.dataCarousel = this.uiDataMapperService.managerCarouselMapper(teams, managers)
     });
-    this.managerSubscription = this.managerService.dataManagersL2$.subscribe({
-      next: (data) => (this.dataManagers = data),
-    });
-
-    if (this.dataTeams && this.dataManagers) {
-      this.dataCarousel = this.uiDataMapperService.managerCarouselMapper(this.dataTeams, this.dataManagers);
-    }
-  }
-
-  ngOnDestroy() {
-    this.teamSubscription?.unsubscribe();
-    this.managerSubscription?.unsubscribe();
   }
 }
