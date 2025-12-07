6e01fecc9e3b1ed2c49088eb3494f54f0b5a554b
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-12-06 23:14:49 -0500

    Refactor TitleComponent template for improved structure and readability

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -7,22 +7,24 @@ import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-title',
   imports: [FontAwesomeModule, RouterLink, RouterLinkActive],
   template: `
-    <div class="w-full h-52 flex flex-col justify-between pt-10 pb-8 bg-crimson text-white background-pattern px-4 sm:px-5 lg:px-16 select-none duration-500">
-      <div class="flex gap-2 font-bold text-xs sm:text-sm items-center duration-500">
-        <div [routerLink]="routeSections[0]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
-          HOME
+    <div class="bg-crimson text-white background-pattern px-3 sm:px-5 duration-500 select-none">
+      <div class="max-w-screen-xl mx-auto h-52 flex flex-col justify-between pt-10 pb-8">
+        <div class="flex gap-2 font-bold text-xs sm:text-sm items-center duration-500">
+          <div [routerLink]="routeSections[0]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
+            HOME
+          </div>
+          <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
+          <div [routerLink]="['/', routeSections[1]]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
+            {{ routeSections[1].toUpperCase() }}
+          </div>
+          <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
+          <div [routerLink]="['/', routeSections[1], routeSections[2]]" routerLinkActive="text-white" class="cursor-pointer hover:opacity-100 hover:duration-200">
+            {{ routeSections[2].toUpperCase() }}
+          </div>
         </div>
-        <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
-        <div [routerLink]="['/', routeSections[1]]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
-          {{ routeSections[1].toUpperCase() }}
+        <div class="font-bold flex md:justify-start items-center duration-500">
+          <p class="font-bold text-6xl">{{ title }}</p>
         </div>
-        <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
-        <div [routerLink]="['/', routeSections[1], routeSections[2]]" routerLinkActive="text-white" class="cursor-pointer hover:opacity-100 hover:duration-200">
-          {{ routeSections[2].toUpperCase() }}
-        </div>
-      </div>
-      <div class="font-bold flex md:justify-start items-center duration-500">
-        <p class="font-bold text-6xl">{{ title }}</p>
       </div>
     </div>
   `,
