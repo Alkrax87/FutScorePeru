59eb72d69c2c1c7c97baace98c85097dc44fa772
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-03-21 10:23:27 -0500

    Refactor CpLeaguesComponent to replace TeamCardCp with LeagueCard for improved consistency in league data representation

--- src/app/pages/copa-peru/cp-leagues/cp-leagues.component.ts
+++ src/app/pages/copa-peru/cp-leagues/cp-leagues.component.ts
@@ -3,18 +3,18 @@ import { FetchLeaguesService } from '../../../services/fetch-leagues.service';
 import { UiDataMapperService } from '../../../services/ui-data-mapper.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TitleComponent } from "../../../components/title/title.component";
-import { TeamCardCpComponent } from "../../../components/team-card-cp/team-card-cp.component";
-import { TeamCardCp } from '../../../interfaces/ui-models/team-card-cp';
+import { LeagueCardComponent } from "../../../components/league-card/league-card.component";
+import { LeagueCard } from '../../../interfaces/ui-models/league-card';
 
 @Component({
   selector: 'app-cp-leagues',
-  imports: [TitleComponent, TeamCardCpComponent],
+  imports: [TitleComponent, LeagueCardComponent],
   template: `
     <app-title [title]="'Ligas'"></app-title>
     <div class="bg-night px-3 sm:px-5 py-10 lg:py-16 duration-500 select-none">
       <div class="grid grid-cols-[repeat(auto-fit,_minmax(310px,_1fr))] gap-3 sm:gap-5 max-w-screen-xl mx-auto duration-500">
         @for (item of dataLeagues; track $index) {
-          <app-team-card-cp [data]="dataLeagues[$index]"></app-team-card-cp>
+          <app-league-card [data]="dataLeagues[$index]"></app-league-card>
         }
       </div>
     </div>
@@ -25,11 +25,11 @@ export class CpLeaguesComponent {
   private leaguesService = inject(FetchLeaguesService);
   private uiDataMapperService = inject(UiDataMapperService);
 
-  dataLeagues: TeamCardCp[] = [];
+  dataLeagues: LeagueCard[] = [];
 
   constructor() {
     this.leaguesService.leagues$.pipe(takeUntilDestroyed()).subscribe({
-      next: (data) => this.dataLeagues = this.uiDataMapperService.leagueCardMapper(data)
+      next: (data) => this.dataLeagues = this.uiDataMapperService.leaguesCardMapper(data)
     });
   }
 }
