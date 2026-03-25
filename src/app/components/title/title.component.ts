182eba039089b9076df6c2dd7376a076807b9f16
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-03-25 10:13:35 -0500

    Add some ui changes

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-title',
@@ -11,14 +11,14 @@ import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
       <div class="max-w-screen-xl mx-auto h-52 flex flex-col justify-between pt-10 pb-8">
         <div class="flex gap-2 font-bold text-xs sm:text-sm items-center duration-500">
           <div [routerLink]="routeSections[0]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
-            HOME
+            <fa-icon [icon]="Home"></fa-icon>
           </div>
           <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
           <div [routerLink]="['/', routeSections[1]]" class="cursor-pointer hover:opacity-100 hover:duration-200 opacity-70">
             {{ routeSections[1].toUpperCase() }}
           </div>
           <fa-icon [icon]="Arrow" class="opacity-70"></fa-icon>
-          <div [routerLink]="['/', routeSections[1], routeSections[2]]" routerLinkActive="text-white" class="cursor-pointer hover:opacity-100 hover:duration-200">
+          <div [routerLink]="['/', routeSections[1], routeSections[2]]" routerLinkActive="text-white" class="cursor-pointer">
             {{ routeSections[2].toUpperCase() }}
           </div>
         </div>
@@ -32,11 +32,13 @@ import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class TitleComponent {
   @Input() title!: string;
+  private router = inject(Router);
 
   routeSections: string[] = [];
-  Arrow = faCaretRight;
+  Arrow = faAngleRight;
+  Home = faHome;
 
-  constructor(private router: Router) {
-    this.routeSections = router.url.split('/');
+  constructor() {
+    this.routeSections = this.router.url.split('/');
   }
 }
