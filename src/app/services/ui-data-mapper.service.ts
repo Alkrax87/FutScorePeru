fb8eff017a78a7921c4a9f69d69b9d5d20cc054a
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-04-05 17:41:11 -0500

    Refactor service to enhance data mapping functions and improve type handling

--- src/app/services/ui-data-mapper.service.ts
+++ src/app/services/ui-data-mapper.service.ts
@@ -1,30 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Team } from '../interfaces/api-models/team';
+import { League } from '../interfaces/api-models/league';
+import { TeamCP } from '../interfaces/api-models/team-cp';
+import { Stadium } from '../interfaces/api-models/stadium';
+import { TeamPerformance } from '../interfaces/api-models/teamPerformance';
+import { TeamForm } from '../interfaces/api-models/teamForm';
+import { Manager } from '../interfaces/api-models/manager';
+import { BracketData } from '../interfaces/api-models/bracket-data';
 import { EntityNav } from '../interfaces/ui-models/entity-nav';
-import { TeamDivision } from '../interfaces/ui-models/team-division';
 import { TeamMap } from '../interfaces/ui-models/team-map';
+import { TeamDivision } from '../interfaces/ui-models/team-division';
 import { TeamCard } from '../interfaces/ui-models/team-card';
+import { LeagueCard } from '../interfaces/ui-models/league-card';
 import { TeamTable } from '../interfaces/ui-models/team-table';
-import { PerformanceData } from '../interfaces/api-models/performance-data';
-import { LastGamesData } from '../interfaces/api-models/last-games-data';
 import { ManagerCarousel } from '../interfaces/ui-models/manager-carousel';
 import { StatisticCard } from '../interfaces/ui-models/statistic-card';
-import { TeamData } from '../interfaces/api-models/team-data';
-import { StadiumData } from '../interfaces/api-models/stadium-data';
-import { ManagerData } from '../interfaces/api-models/manager-data';
-import { LeagueData } from '../interfaces/api-models/league-data';
-import { TeamCardCp } from '../interfaces/ui-models/team-card-cp';
-import { TeamCPData } from '../interfaces/api-models/team-cp-data';
 import { MatchCard } from '../interfaces/ui-models/match-card';
-import { BracketData } from '../interfaces/api-models/bracket-data';
-import { TeamCompactTable } from '../interfaces/ui-models/team-compact-table';
+import { TeamMatchResults } from '../interfaces/api-models/teamMatchResults';
+import { NextMatch } from '../interfaces/ui-models/team-overview';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UiDataMapperService {
-  constructor() {}
-
-  teamsNavMapper(dataTeams: TeamData[]): EntityNav[] {
+  // ================================================
+  // ================= Entity Nav ===================
+  // ================================================
+  teamsNavMapper(dataTeams: Team[]): EntityNav[] {
     const newData = [];
 
     for (const team of dataTeams) {
@@ -38,8 +40,24 @@ export class UiDataMapperService {
 
     return newData;
   }
+  leaguesNavMapper(dataLeagues: League[]): EntityNav[] {
+    const newData = [];
 
-  teamMapMapper(dataTeams: TeamData[]): TeamMap[]  {
+    for (const league of dataLeagues) {
+      newData.push({
+        leagueId: league.leagueId,
+        imageThumbnail: league.imageThumbnail,
+        alt: league.alt
+      });
+    }
+
+    return newData;
+  }
+
+  // ================================================
+  // ===================== Map ======================
+  // ================================================
+  teamsMapMapper(dataTeams: Team[]): TeamMap[]  {
     const newData = [];
 
     for (const team of dataTeams) {
@@ -54,8 +72,25 @@ export class UiDataMapperService {
 
     return newData;
   }
+  teamsCPMapMapper(dataTeams: TeamCP[]): TeamMap[] {
+    const newData = [];
 
-  teamDivisionMapper(dataTeams: TeamData[]): TeamDivision[] {
+    for (const [index, teamCP] of dataTeams.entries()) {
+      newData.push({
+        teamId: teamCP.teamId,
+        imageThumbnail: teamCP.image ? teamCP.image : 'assets/images/pages/no-team.webp',
+        alt: 'CPTeam' + (index + 1),
+        location: teamCP.location,
+      });
+    }
+
+    return newData;
+  }
+
+  // ================================================
+  // =============== Division Teams =================
+  // ================================================
+  teamsDivisionMapper(dataTeams: Team[]): TeamDivision[] {
     const newData = [];
 
     for (const team of dataTeams) {
@@ -69,40 +104,25 @@ export class UiDataMapperService {
 
     return newData;
   }
-
-  teamDivisionMapperCP(dataTeams: TeamCPData[]): TeamDivision[] {
+  teamsCPDivisionMapper(dataTeams: TeamCP[]): TeamDivision[] {
     const newData = [];
 
-    for (const team of dataTeams) {
+    for (const [index, team] of dataTeams.entries()) {
       newData.push({
-        imageThumbnail: team.image,
+        imageThumbnail: team.image ? team.image : 'assets/images/pages/no-team.webp',
+        alt: 'CPTeam' + (index + 1),
       });
     }
 
     return newData;
   }
 
-  teamCPMapMapper(dataTeams: TeamCPData[]): TeamMap[] {
+  // ================================================
+  // ================= Teams Card ===================
+  // ================================================
+  teamsCardMapper(dataTeams: Team[], dataStadiums: Stadium[]): TeamCard[] {
     const newData = [];
-
-    for (const teamCP of dataTeams) {
-      newData.push({
-        category: 4,
-        teamId: teamCP.teamId,
-        imageThumbnail: teamCP.image,
-        alt: teamCP.abbreviation + '-logo',
-        location: teamCP.location,
-      });
-    }
-
-    return newData;
-  }
-
-  teamCardMapper(dataTeams: TeamData[], dataStadiums: StadiumData[]): TeamCard[] {
-    const newData = [];
-    const stadiumMap = new Map(
-      dataStadiums.map((stadium) => [stadium.stadiumId, stadium])
-    );
+    const stadiumMap = new Map(dataStadiums.map((stadium) => [stadium.stadiumId, stadium]));
 
     for (const team of dataTeams) {
       const stadium = stadiumMap.get(team.stadium);
@@ -113,11 +133,11 @@ export class UiDataMapperService {
         name: team.name,
         image: team.image,
         alt: team.alt,
+        location: team.location,
         color: team.color,
         stadium: {
           name: stadium?.name ?? 'Por Definir',
           capacity: stadium?.capacity ?? 0,
-          location: stadium?.location ?? '',
         },
       });
     }
@@ -125,146 +145,15 @@ export class UiDataMapperService {
     return newData;
   }
 
-  teamsTableMapper(dataTeams: TeamData[], dataPerformance: PerformanceData[], dataLastGames: LastGamesData[], phaseKey: string, teamGroup?: string, activePhase?: string): TeamTable[] {
-    const newData: TeamTable[] = [];
-    let phaseLastGames = phaseKey;
-
-    if (activePhase !== undefined && activePhase !== null && activePhase !== '') {
-      phaseLastGames = activePhase;
-    }
-
-    const performanceMap = new Map(
-      dataPerformance.map((performance) => [performance.teamId, performance[phaseKey as keyof PerformanceData]])
-    );
-    const lastGamesMap = new Map(
-      dataLastGames.map((lastGames) => [lastGames.teamId, lastGames[phaseLastGames as keyof LastGamesData]]),
-    );
-
-    for (const team of dataTeams) {
-      if (team.groupFirstPhase === teamGroup || team.groupSecondPhase === teamGroup || !teamGroup) {
-        const teamPerformance = performanceMap.get(team.teamId) as TeamTable['performance'];
-        const teamLastGames = lastGamesMap.get(team.teamId) as string[];
-
-        newData.push({
-          category: team.category,
-          teamId: team.teamId,
-          name: team.name,
-          abbreviation: team.abbreviation,
-          imageThumbnail: team.imageThumbnail,
-          alt: team.alt,
-          performance: teamPerformance,
-          lastgames: teamLastGames ? teamLastGames.slice(-5) : ['','','','',''],
-        });
-      }
-    }
-
-    return newData.sort(
-      (a, b) =>
-        b.performance.points - a.performance.points ||
-        b.performance.gd - a.performance.gd ||
-        b.performance.gf - a.performance.gf
-    );
-  }
-
-  teamsTableCompactMapper(dataTeams: TeamData[], dataPerformance: PerformanceData[], phaseKey: string, teamGroup?: string): TeamCompactTable[] {
-    const newData: TeamCompactTable[] = [];
-
-    const performanceMap = new Map(
-      dataPerformance.map((performance) => [performance.teamId, performance[phaseKey as keyof PerformanceData]])
-    );
-
-    for (const team of dataTeams) {
-      if (team.groupFirstPhase === teamGroup || team.groupSecondPhase === teamGroup || !teamGroup) {
-        const teamPerformance = performanceMap.get(team.teamId) as TeamCompactTable['performance'] | undefined;
-
-        newData.push({
-          category: team.category,
-          teamId: team.teamId,
-          name: team.name,
-          abbreviation: team.abbreviation,
-          imageThumbnail: team.imageThumbnail,
-          alt: team.alt,
-          performance: {
-            points: teamPerformance?.points ?? 0,
-            gf: teamPerformance?.gf ?? 0,
-            gd: teamPerformance?.gd ?? 0,
-          },
-        });
-      }
-    }
-
-    return newData.sort(
-      (a, b) =>
-        b.performance.points - a.performance.points ||
-        b.performance.gd - a.performance.gd ||
-        b.performance.gf - a.performance.gf
-    );
-  }
-
-  managerCarouselMapper(dataTeams: TeamData[], dataManagers: ManagerData[]): ManagerCarousel[] {
-    const newData = [];
-
-    for (const team of dataTeams) {
-      const managers = dataManagers.filter(
-        (manager) => manager.teamId === team.teamId
-      );
-
-      newData.push({
-        category: team.category,
-        teamId: team.teamId,
-        name: team.name,
-        image: team.image,
-        alt: team.alt,
-        manager: managers,
-      });
-    }
-
-    return newData;
-  }
-
-  statisticsCardMapper(dataTeams: TeamData[], statistics: any, valueKey: string): StatisticCard[] {
-    const teamMap = new Map(
-      dataTeams.map((team) => [team.teamId, team])
-    );
-
-    return statistics.map((element: any) => {
-      const team = teamMap.get(element.teamId);
-
-      return {
-        category: team?.category,
-        teamId: element.teamId,
-        name: team?.name,
-        abbreviation: team?.abbreviation,
-        image: team?.image,
-        imageThumbnail: team?.imageThumbnail,
-        alt: team?.alt,
-        value: element[valueKey],
-      };
-    });
-  }
-
-  leagueNavMapper(dataLeagues: LeagueData[]): EntityNav[] {
-    const newData = [];
-
-    for (const league of dataLeagues) {
-      newData.push({
-        category: league.category,
-        leagueId: league.leagueId,
-        imageThumbnail: league.imageThumbnail,
-        alt: league.alt
-      });
-    }
-
-    return newData;
-  }
-
-  leagueCardMapper(dataLeague: LeagueData[]): TeamCardCp[] {
+  // ================================================
+  // ================= League Card ==================
+  // ================================================
+  leaguesCardMapper(dataLeague: League[]): LeagueCard[] {
     const newData = [];
 
     for (const league of dataLeague) {
       newData.push({
         leagueId: league.leagueId,
-        category: league.category,
         region: league.location,
         flag: league.image,
         color: {
@@ -282,11 +171,99 @@ export class UiDataMapperService {
     return newData;
   }
 
-  bracketCardMapper(dataTeams: TeamCPData[] | TeamData[], bracketData: BracketData[]): MatchCard[] {
-    const newData: MatchCard[] = [];
-    const teamMap = new Map(
-      dataTeams.map((team) => [team.teamId, team])
+  // ================================================
+  // ==================== Table =====================
+  // ================================================
+  teamsTableMapper(dataTeams: Team[], dataTeamsPerformance: TeamPerformance[], dataTeamsForm: TeamForm[], phase: 'overall' | 'phase1' | 'phase2', teamGroup?: string, activePhase?: 'phase1' | 'phase2'): TeamTable[] {
+    const newData: TeamTable[] = [];
+    let formPhase = phase;
+
+    if (activePhase !== undefined) { formPhase = activePhase }
+
+    const performanceMap = new Map(dataTeamsPerformance.map((performance) => [performance.teamId, performance[phase as keyof TeamPerformance]]));
+    const formMap = new Map(dataTeamsForm.map((form) => [form.teamId, form[formPhase as keyof TeamForm]]));
+
+    for (const team of dataTeams) {
+      if (team.groupPhase1 === teamGroup || team.groupPhase2 === teamGroup || !teamGroup) {
+        const teamPerformance = performanceMap.get(team.teamId) as TeamTable['performance'];
+        const teamForm = formMap.get(team.teamId) as string[];
+
+        newData.push({
+          category: team.category,
+          teamId: team.teamId,
+          name: team.name,
+          abbreviation: team.abbreviation,
+          imageThumbnail: team.imageThumbnail,
+          alt: team.alt,
+          form: teamForm ? teamForm.slice(-5) : ['','','','',''],
+          performance: teamPerformance ?? { points: 0, played: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0 },
+        });
+      }
+    }
+
+    return newData.sort((a, b) =>
+      b.performance.points - a.performance.points ||
+      b.performance.gd - a.performance.gd ||
+      b.performance.gf - a.performance.gf
     );
+  }
+
+  // ================================================
+  // =============== Managers Carousel ==============
+  // ================================================
+  managersCarouselMapper(dataTeams: Team[], dataManagers: Manager[]): ManagerCarousel[] {
+    const newData = [];
+
+    for (const team of dataTeams) {
+      const managers = dataManagers.filter((manager) => manager.teamId === team.teamId);
+
+      newData.push({
+        category: team.category,
+        teamId: team.teamId,
+        name: team.name,
+        imageThumbnail: team.imageThumbnail,
+        alt: team.alt,
+        managers: managers,
+      });
+    }
+
+    return newData;
+  }
+
+  // ================================================
+  // ================ Statistics Card ===============
+  // ================================================
+  statisticsCardMapper(dataTeams: Team[], dataStatistics: any, valueKey: string): StatisticCard[] {
+    const newData = [];
+
+    const teamMap = new Map(dataTeams.map((team) => [team.teamId, team]));
+
+    for (const item of dataStatistics) {
+      const team = teamMap.get(item.teamId);
+
+      if (team) {
+        newData.push({
+          category: team.category,
+          teamId: item.teamId,
+          name: team.name,
+          abbreviation: team.abbreviation,
+          image: team.image,
+          imageThumbnail: team.imageThumbnail,
+          alt: team.alt,
+          value: item[valueKey],
+        });
+      }
+    }
+
+    return newData;
+  }
+
+  // ================================================
+  // ================= Brackeys Card ================
+  // ================================================
+  bracketsCardMapper(dataTeams: TeamCP[] | Team[], bracketData: BracketData[]): MatchCard[] {
+    const newData: MatchCard[] = [];
+    const teamMap = new Map(dataTeams.map((team) => [team.teamId, team]));
 
     bracketData.forEach(bracket => {
       const team1 = teamMap.get(bracket.teamA.teamId);
@@ -321,5 +298,98 @@ export class UiDataMapperService {
     });
 
     return newData;
+  }
+
+  // ================================================
+  // =============== Overview Latest 5 ==============
+  // ================================================
+  overviewLatest5Mapper(dataTeams: Team[], fixture: { round: number, home: string, away: string, postponed: boolean, date: Date | null }[], teamsMatchResults: TeamMatchResults[], teamId: string) {
+    const newData = [];
+
+    const teamMap = new Map(dataTeams.map((team) => [team.teamId, team]));
+    const teamMatchResultsMap = new Map(teamsMatchResults.map((matchResult: TeamMatchResults) => [matchResult.teamId, matchResult]));
+
+    for (const match of fixture) {
+      const homeTeam = teamMap.get(match.home);
+      const awayTeam = teamMap.get(match.away);
+
+      if (homeTeam && awayTeam) {
+        const homeResults = teamMatchResultsMap.get(homeTeam.teamId);
+        const awayResults = teamMatchResultsMap.get(awayTeam.teamId);
+
+        const resultHome = homeResults?.phase1[match.round - 1] ?? "";
+        const resultAway = awayResults?.phase1[match.round - 1] ?? "";
+
+        if (homeTeam.teamId === teamId) {
+          newData.push({
+            round: match.round,
+            postponed: match.postponed,
+            date: match.date,
+            rivalTeamId: awayTeam.teamId,
+            rivalTeamLogo: awayTeam.image,
+            rivalTeamAlt: awayTeam.alt,
+            homeTeamScore: resultHome,
+            awayTeamScore: resultAway,
+            home: true,
+          });
+        } else if (awayTeam.teamId === teamId) {
+          newData.push({
+            round: match.round,
+            postponed: match.postponed,
+            date: match.date,
+            rivalTeamId: homeTeam.teamId,
+            rivalTeamLogo: homeTeam.image,
+            rivalTeamAlt: homeTeam.alt,
+            homeTeamScore: resultHome,
+            awayTeamScore: resultAway,
+          });
+        }
+      } else {
+        newData.push({
+          round: match.round,
+          postponed: match.postponed,
+          date: match.date,
+          homeTeamLogo: teamMap.get(match.home)?.image,
+          homeTeamAlt: teamMap.get(match.home)?.alt,
+          free: true,
+          home: true,
+        });
+      }
+    }
+
+    return newData;
+  }
+
+  // ================================================
+  // ============== Overview Next Match =============
+  // ================================================
+  overviewNextMatchMapper(dataTeams: Team[], nextMatchData: { round: number, home: string, away: string, postponed: boolean, date: Date | null }): NextMatch {
+    const teamMap = new Map(dataTeams.map((team) => [team.teamId, team]));
+
+    const homeTeam = teamMap.get(nextMatchData.home);
+    const awayTeam = teamMap.get(nextMatchData.away);
+
+    if (homeTeam && awayTeam) {
+      return {
+        round: nextMatchData.round,
+        homeTeamId: homeTeam.teamId,
+        awayTeamId: awayTeam.teamId,
+        homeTeamName: homeTeam.name,
+        awayTeamName: awayTeam.name,
+        homeTeamAbbreviation: homeTeam.abbreviation,
+        awayTeamAbbreviation: awayTeam.abbreviation,
+        homeTeamImage: homeTeam.image,
+        awayTeamImage: awayTeam.image,
+        homeTeamAlt: homeTeam.alt,
+        awayTeamAlt: awayTeam.alt,
+        postponed: nextMatchData.postponed,
+        date: nextMatchData.date,
+        valid: true,
+      }
+    }
+
+    return {
+      valid: false,
+    }
   }
 }
